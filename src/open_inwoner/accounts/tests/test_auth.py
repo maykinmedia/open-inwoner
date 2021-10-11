a0538b3f7374e49f048221920a754bac2facdb12
Vasileios Zigras <zigras00@gmail.com>
2021-10-11 17:03:01 +0200

    added registration tests and refactored code

--- src/open_inwoner/accounts/tests/test_auth.py
+++ src/open_inwoner/accounts/tests/test_auth.py
@@ -15,15 +15,8 @@ TEST_USER = {
 }
 
 
-class AuthTests(WebTest):
-    def setUp(self):
-        # Create a user. We need to reset their password
-        # because otherwise we do not have access to the raw one associated.
-        self.user = UserFactory.create(password="test")
-
-    def test_registration(self):
-        """Test user registration succeeds."""
-
+class TestRegistrationFunctionality(WebTest):
+    def test_registration_succeeds_with_right_user_input(self):
         register_page = self.app.get(reverse("django_registration_register"))
 
         form = register_page.forms["registration-form"]
@@ -38,6 +31,41 @@ class AuthTests(WebTest):
         registered_user = User.objects.get(email=TEST_USER["email"])
         self.assertEquals(registered_user.email, TEST_USER["email"])
         self.assertTrue(registered_user.check_password(TEST_USER["password"]))
+
+    def test_registration_fails_without_filling_out_first_name(self):
+        register_page = self.app.get(reverse("django_registration_register"))
+
+        form = register_page.forms["registration-form"]
+        form["email"] = TEST_USER["email"]
+        form["last_name"] = TEST_USER["last_name"]
+        form["password1"] = TEST_USER["password"]
+        form["password2"] = TEST_USER["password"]
+        form.submit()
+
+        # Verify that the user has not been registered
+        user_query = User.objects.filter(email=TEST_USER["email"])
+        self.assertEquals(user_query.count(), 0)
+
+    def test_registration_fails_without_filling_out_last_name(self):
+        register_page = self.app.get(reverse("django_registration_register"))
+
+        form = register_page.forms["registration-form"]
+        form["email"] = TEST_USER["email"]
+        form["first_name"] = TEST_USER["first_name"]
+        form["password1"] = TEST_USER["password"]
+        form["password2"] = TEST_USER["password"]
+        form.submit()
+
+        # Verify that the user has not been registered
+        user_query = User.objects.filter(email=TEST_USER["email"])
+        self.assertEquals(user_query.count(), 0)
+
+
+class TestLoginLogoutFunctionality(WebTest):
+    def setUp(self):
+        # Create a user. We need to reset their password
+        # because otherwise we do not have access to the raw one associated.
+        self.user = UserFactory.create(password="test")
 
     def test_login(self):
         """Test that a user is successfully logged in."""
