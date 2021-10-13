27955fa31bafb13278ec6934227eebec7b830d9d
Anna Shamray <anna.shamray@maykinmedia.nl>
2021-10-13 16:04:06 +0200

    :sparkles: [#86] add Category model

--- src/open_inwoner/pdc/admin.py
+++ src/open_inwoner/pdc/admin.py
@@ -3,10 +3,26 @@ from django.contrib import admin
 from treebeard.admin import TreeAdmin
 from treebeard.forms import movenodeform_factory
 
-from .models import Category
+from .models import Category, Product
 
 
 @admin.register(Category)
 class CategoryAdmin(TreeAdmin):
     form = movenodeform_factory(Category, fields="__all__")
     prepopulated_fields = {"slug": ("name",)}
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ("name", "created_on", "display_categories")
+    list_filter = ("categories",)
+    date_hierarchy = "created_on"
+
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        return qs.prefetch_related("categories")
+
+    def display_categories(self, obj):
+        return ", ".join(p.name for p in obj.categories.all())
+
+    display_categories.short_description = "categories"
