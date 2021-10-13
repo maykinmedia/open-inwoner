1e45320a41d18150790d2d7f8ac3cc5d1ee97451
Jorik Kraaikamp <jorikkraaikamp@gmail.com>
2021-10-13 16:55:53 +0200

    removed readded code

--- src/open_inwoner/utils/views.py
+++ src/open_inwoner/utils/views.py
@@ -5,10 +5,6 @@ from django.views import View
 from django.views.decorators.csrf import requires_csrf_token
 from django.views.defaults import ERROR_500_TEMPLATE_NAME
 
-from django_sendfile import sendfile
-
-from .storage import private_storage
-
 
 @requires_csrf_token
 def server_error(request, template_name=ERROR_500_TEMPLATE_NAME):
@@ -29,12 +25,3 @@ def server_error(request, template_name=ERROR_500_TEMPLATE_NAME):
         )
     context = {"request": request}
     return http.HttpResponseServerError(template.render(context))
-
-
-class DownloadExportView(View):
-    def get(self, request, path):
-        if request.user.is_authenticated:
-            fs_path = private_storage.path(path)
-            return sendfile(request, fs_path, attachment=True)
-
-        raise PermissionDenied
