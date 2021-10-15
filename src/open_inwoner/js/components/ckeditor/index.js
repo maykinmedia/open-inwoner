2365308f662295aa2bf6fe1914b1a1021d6105dc
Anna Shamray <anna.shamray@maykinmedia.nl>
2021-10-15 18:32:29 +0200

    :fire: remove code for debugging

--- src/open_inwoner/js/components/ckeditor/index.js
+++ src/open_inwoner/js/components/ckeditor/index.js
@@ -6,8 +6,5 @@ const SELECTOR = '.ckeditor-selection';
 const nodes = document.querySelectorAll(SELECTOR);
 
 for (const node of nodes) {
-    console.log(node);
-    // const props = node.dataset;
-    // ReactDOM.render(<AlfrescoDocumentSelection {...props} />, node);
     getEditor(node);
 }
