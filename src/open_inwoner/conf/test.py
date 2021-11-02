7deb308a4479f5ffc8af1a78bb25c25c01207de8
Jorik Kraaikamp <jorikkraaikamp@gmail.com>
2021-11-02 09:15:27 +0100

    should be mock backend

--- src/open_inwoner/conf/test.py
+++ src/open_inwoner/conf/test.py
@@ -8,6 +8,14 @@ os.environ.setdefault("ENVIRONMENT", "test")
 # NOTE: watch out for multiple projects using the same cache!
 os.environ.setdefault("CACHE_DEFAULT", "127.0.0.1:6379/0")
 
+# Allow logging in with both username+password and email+password
+AUTHENTICATION_BACKENDS = [
+    "open_inwoner.accounts.backends.CustomAxesBackend",
+    "open_inwoner.accounts.backends.UserModelEmailBackend",
+    "django.contrib.auth.backends.ModelBackend",
+    "digid_eherkenning.mock.backends.DigiDBackend",
+]
+
 from .production import *  # noqa isort:skip
 
 for db_alias in DATABASES.keys():
