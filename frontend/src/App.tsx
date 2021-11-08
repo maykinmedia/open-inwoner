bbda89e5d1f2f70d66c75a7dade45c59f08fe5b7
Sven van de Scheur <svenvandescheur@gmail.com>
2021-11-08 17:44:21 +0100

    :sparkles: #120 - Create footer.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {Header} from './Components/Header/Header'
 import {Container} from './Components/Container/Container';
+import {Footer} from './Components/Footer/Footer';
 import {RouterView} from './routes/RouterView';
 import './fonts/TheMixC5/DesktopFonts/TheMixC5-5_Plain.otf';
 import './fonts/TheSansC5/DesktopFonts/TheSansC5-5_Plain.otf';
@@ -9,15 +10,11 @@ import './App.scss';
 export function App() {
   return (
     <>
-      <Header>
-
-        <div className="menu__info">
-
-        </div>
-      </Header>
+      <Header/>
       <Container>
         <RouterView/>
       </Container>
+      <Footer/>
     </>
   );
 }
