eb7fb7d082db192892fc820d8897b6fb028323e8
Sven van de Scheur <svenvandescheur@gmail.com>
2021-11-10 15:09:00 +0100

    :sparkles: #118 - Create accessibility header.

--- frontend/src/Components/Typography/Link.tsx
+++ frontend/src/Components/Typography/Link.tsx
@@ -1,4 +1,4 @@
-import React, {ComponentType} from 'react';
+import React, {ComponentType, ReactElement} from 'react';
 import {NavLink} from 'react-router-dom';
 import './Link.scss';
 
@@ -16,17 +16,35 @@ interface iLinkProps {
 /**
  * Generic link component.
  * @param {iLinkProps} props
- * @return {JSX.Element}
+ * @return {ReactElement}
  */
-export const Link = (props: iLinkProps) => {
+export function Link(props: iLinkProps): ReactElement {
   const {to, activeClassName, children, icon, shouldRenderIcon, ..._props} = props;
   const className = (props.primary) ? 'link link--primary' : 'link';
   const Icon = icon;
 
-  return (
-    <NavLink className={className} activeClassName={activeClassName} to={to} {..._props as any}>
+  /**
+   * Renders the children of the link.
+   */
+  const renderChildren = () => (
+    <>
       {shouldRenderIcon && Icon && <Icon/>}
       {(shouldRenderIcon && Icon) ? children : children || to}
+    </>
+  );
+
+  // Special cases.
+  if (to.match('^#') || to.match(':')) {
+    return (
+      <a className={className} href={to} {..._props as any}>
+        {renderChildren()}
+      </a>
+    )
+  }
+
+  return (
+    <NavLink className={className} activeClassName={activeClassName} to={to} {..._props as any}>
+      {renderChildren()}
     </NavLink>
   );
 }
