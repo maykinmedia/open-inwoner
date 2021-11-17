471c9efec277cc1c7df5a5702b0144e4a7e5f444
Anna Shamray <anna.shamray@maykinmedia.nl>
2021-11-17 17:25:18 +0100

    :sparkles: [#78] add faceted search on products

--- src/open_inwoner/search/searches.py
+++ src/open_inwoner/search/searches.py
@@ -1,9 +1,26 @@
+from django.conf import settings
+
+from elasticsearch_dsl import FacetedSearch, NestedFacet, TermsFacet
+
 from .documents import ProductDocument
 
 
-def search_products(value: str):
-    s = ProductDocument.search()
-    s = s.query("multi_match", query=value, fields=["name^4", "summary", "content"])
-    result = s.execute()
+class ProductSearch(FacetedSearch):
+    index = settings.ES_INDEX_PRODUCTS
+    doc_types = [ProductDocument]
+    fields = ["name^4", "summary", "content"]
+    facets = {
+        "categories": NestedFacet(
+            "categories", TermsFacet(field="categories.name.raw")
+        ),
+        "tags": NestedFacet("tags", TermsFacet(field="tags.name.raw")),
+        "organizations": NestedFacet(
+            "organizations", TermsFacet(field="organizations.name.raw")
+        ),
+    }
 
+
+def search_products(query: str, filters=None):
+    s = ProductSearch(query, filters=filters or {})
+    result = s.execute()
     return result.hits
