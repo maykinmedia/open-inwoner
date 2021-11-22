5a464a518059d3ee3411d0f5bdfbdee33a1b1fe6
Vasileios Zigras <zigras00@gmail.com>
2021-11-22 09:31:04 +0100

    [#112] Fixed cronjob,added staff user check to the api

--- src/open_inwoner/api/accounts/views/deactivate.py
+++ src/open_inwoner/api/accounts/views/deactivate.py
@@ -7,9 +7,15 @@ from open_inwoner.accounts.models import User
 
 
 class DeactivateUserView(UpdateAPIView):
+    def put(self, request, *args, **kwargs):
+        return Response({"detail": 'Methode "PUT" not allowed.'}, status=405)
+
     def patch(self, request, *args, **kwargs):
         user = User.objects.get(id=self.request.user.id)
+        if user.is_staff:
+            return Response({"detail": "Staff user cannot be deactivated."}, status=403)
+
         user.is_active = False
         user.deactivated_on = date.today()
         user.save()
-        return Response({"detail": "user has been deactivated"}, status=200)
+        return Response({"detail": "User has been deactivated."}, status=200)
