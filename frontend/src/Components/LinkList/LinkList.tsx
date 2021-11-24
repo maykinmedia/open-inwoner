437445e8242c13d3af63f44c420c54e81f4c9882
Jorik Kraaikamp <jorikkraaikamp@gmail.com>
2021-11-24 09:34:04 +0100

    Added all other elements for searching
    Problem with the search results

--- frontend/src/Components/LinkList/LinkList.tsx
+++ frontend/src/Components/LinkList/LinkList.tsx
@@ -1,23 +1,41 @@
 import React from 'react';
 
 import { Link } from 'react-router-dom'
+import { H1 } from '../Typography/H1'
 
 import './LinkList.scss'
 
 interface iLinkForList {
   name: string,
+  intro: string,
   href: string,
   type: string,
 }
 
 interface iLinkListProps {
+  count: number,
   links: Array<iLinkForList>,
 }
 
 export function LinkList(props: iLinkListProps) {
+  const getResult = (link:iLinkForList, index:number) => {
+    return (
+      <Link className="search-results__item" key={index} to={link.href}>
+        <p className="search-results__item-title">{link.name}</p>
+        <div className="search-results__item-info-container">
+          <p className="search-results__item-intro">{link.intro}</p>
+          <p className="search-results__item-type">{link.type}</p>
+        </div>
+      </Link>
+    )
+  }
+
   return (
-    <ul className="link-list">
-      {props.links.map((link, index) => <li key={ index }><Link to={link.href}>{link.name}</Link></li>)}
-    </ul>
+    <div className="search-results">
+      <H1 className="search-results__title">Zoekresultaten<span className="search-results__title-small">{ props.count } zoekresultaten</span></H1>
+      <div className="search-results__list">
+        {props.links.map((link, index) => getResult(link, index))}
+      </div>
+    </div>
   );
 }
--- frontend/src/pages/Search.tsx
+++ frontend/src/pages/Search.tsx
@@ -1,4 +1,4 @@
-import React, {SyntheticEvent, useState, ReactElement} from 'react';
+import React, {SyntheticEvent, ChangeEvent, useState, ReactElement, useEffect} from 'react';
 
 import { Grid } from '../Components/Container/Grid';
 import { LinkList } from '../Components/LinkList/LinkList';
@@ -8,27 +8,55 @@ import { P } from '../Components/Typography/P';
 import { Divider } from '../Components/Divider/Divider';
 import { Button } from '../Components/Button/Button';
 import { ButtonRow } from '../Components/Button/ButtonRow';
+import { Pagination } from '../Components/Pagination/Pagination';
 
 import {search} from '../api/calls';
 
 import { Filter } from '../Components/Filter/Filter'
 import {iField} from '../types/field';
-import { iProduct } from '../types/pdc';
+import { iFacet, iSearchResult } from '../types/search';
+
+interface iFilters {
+  categories?: Array<string>,
+  tags?: Array<string>,
+  organizations?: Array<string>,
+}
 
 export default function Search() {
-  const [products, setProducts] = useState<iProduct[]>([]);
+  const [searchResults, setSearchResults] = useState<iSearchResult[]>([]);
+  const [page, setPage] = useState<number>(1);
+  const [resultCount, setResultCount] = useState<number>(0);
+  const [facets, setFacets] = useState<iFacet[]>([]);
   const [searchTerms, setSearchTerms] = useState<Array<string>>([]);
-  // const [filters, setFilters] = useState<any>([]);
+  const [filters, setFilters] = useState<iFilters>({
+    categories: [],
+    tags: [],
+    organizations: [],
+  });
 
-  const onSubmit = async (event: SyntheticEvent, data: { query: string }): Promise<void> => {
+  useEffect(() => {
+    const load = async () => {
+      await onSubmit(null, {query: searchTerms.join(" ")})
+    };
+    load();
+  }, [filters, page]);
+
+  const onSubmit = async (event: SyntheticEvent | null, data: { query: string }): Promise<void> => {
     const {query} = data;
 
-    event.preventDefault();
-    const returnedProducts = await search(query);
-    setProducts(returnedProducts)
+    event?.preventDefault();
+    const localSearchResults = await search(page, filters, query);
+    if (true) {
+      setSearchResults(localSearchResults.results);
+      setResultCount(localSearchResults.count);
+    } else {
+      setResultCount(0);
+      setSearchResults([]);
+    }
+    setFacets(localSearchResults.facets);
   };
 
-  const searchChange = (event:SyntheticEvent) => {
+  const searchChange = (event:ChangeEvent) => {
     setSearchTerms(event.currentTarget.value.split(" "));
   }
 
@@ -37,11 +65,68 @@ export default function Search() {
     let localSearchTerms = [...searchTerms]
     localSearchTerms.splice(index, 1);
     setSearchTerms(localSearchTerms);
+    await onSubmit(null, {query: searchTerms.join(" ")})
+  }
+
+  const toggleFilter = async (event: SyntheticEvent) => {
+    const slug = event.currentTarget.id;
+    const facetName: string = event.currentTarget.name;
+    const checked = event.currentTarget.checked;
+
+    const removeValue = async (list: Array<string>, value: string) => {
+      const index = list.indexOf(value);
+      if (index != null && index > -1) {
+        list.splice(index, 1);
+      }
+    }
+
+    let localFilters: iFilters = {
+      categories: [],
+      tags: [],
+      organizations: [],
+    }
+    Object.assign(localFilters, filters);
+
+    if (facetName === "categories") {
+      if (checked) {
+        localFilters.categories?.push(slug);
+      } else {
+        await removeValue(localFilters.categories, slug);
+      }
+    }
+    if (facetName === "tags") {
+      if (checked) {
+        localFilters.tags?.push(slug);
+      } else {
+        await removeValue(localFilters.tags, slug);
+      }
+    }
+    if (facetName === "organizations") {
+      if (checked) {
+        localFilters.organizations?.push(slug);
+      } else {
+        await removeValue(localFilters.organizations, slug);
+      }
+    }
+    await setFilters(localFilters);
+    // await onSubmit(null, {query: searchTerms.join(" ")})
+  }
+
+  const changePage = async (event: SyntheticEvent) => {
+    setPage(event.currentTarget.dataset.page);
+  }
+
+  const prevPage = async (event: SyntheticEvent) => {
+    setPage(page - 1);
+  }
+
+  const nextPage = async (event: SyntheticEvent) => {
+    setPage(page + 1);
   }
 
   const getFields = (): iField[] => [
     {
-      label: 'Zoek op trefwoord', name: 'query', type: 'text', value: searchTerms.join(" "), onChange: searchChange
+      label: 'Zoek op trefwoord', name: 'query', type: 'text', value: searchTerms.join(" "), onChange: searchChange, required: false
     },
   ];
 
@@ -66,53 +151,30 @@ export default function Search() {
   )
 
   const displayResults = () => {
-    let links = products.map((product) => {
+    let links = searchResults.map((searchResult) => {
       return {
-        name: product.name,
-        href: `/products/${product.slug}`,
+        name: searchResult.name,
+        intro: searchResult.summary,
+        href: `/products/${searchResult.slug}`,
         type: "product"
       }
     });
-    return <LinkList links={links} />
+    return <LinkList links={links} count={resultCount} />
   }
 
   const getMainContent = (): ReactElement => (
     <>
       {displayResults()}
+      <Pagination prevAction={prevPage} nextAction={nextPage} defaultAction={changePage} currentPage={page} itemsPerPage={10} totalItems={100} />
     </>
   );
-
-  const filterItems = [
-    { slug: 'test1', name: "test", count: 0, selected: true },
-    { slug: 'test2', name: "test2", count: 0, selected: false },
-    { slug: 'test3', name: "test3", count: 0, selected: false },
-    { slug: 'test4', name: "test4", count: 0, selected: false },
-    { slug: 'test5', name: "test5", count: 0, selected: false },
-    { slug: 'test6', name: "test6", count: 0, selected: false },
-    { slug: 'test7', name: "test7", count: 0, selected: false },
-    { slug: 'test8', name: "test9", count: 0, selected: false },
-    { slug: 'test9', name: "test8", count: 0, selected: false },
-    { slug: 'test10', name: "test10", count: 0, selected: false },
-    { slug: 'test11', name: "test11", count: 0, selected: false },
-  ]
-
-  const facets = [{
-    name: "Filter op thema",
-    buckets: filterItems,
-  }, {
-    name: "Filter op tag",
-    buckets: filterItems,
-  }, {
-    name: "Filter op organisatie",
-    buckets: filterItems,
-  }];
 
   const getSidebarContent = (): ReactElement => (
     <>
       {facets?.map((facet, index) => {
         return (
           <div key={ index }>
-            <Filter items={facet.buckets} title={ facet.name } />
+            <Filter items={facet.buckets} facet={facet} action={toggleFilter} />
             <Divider small={ true }></Divider>
           </div>
         )
