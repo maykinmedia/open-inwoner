663bc69d8ab2e82e5f0f068e8c50c4f4d990af58
Jorik Kraaikamp <jorikkraaikamp@gmail.com>
2021-11-24 14:38:58 +0100

    Logo errror

--- frontend/src/Components/Logo/Logo.tsx
+++ frontend/src/Components/Logo/Logo.tsx
@@ -9,15 +9,15 @@ export function Logo() {
   const { globalState } = useContext(globalContext);
 
   const getLogo = () => {
-    if (globalState.logo) {
-      return `${import.meta.env.VITE_API_URL}${globalState.logo.file}`
+    if (globalState?.logo) {
+      return `${import.meta.env.VITE_API_URL}${globalState?.logo.file}`
     }
     return ""
   }
 
   return (
     <Link className="logo" to="/">
-      <img className="logo__image" src={getLogo()} alt={ globalState.logo?.description }/>
+      <img className="logo__image" src={getLogo()} alt={ globalState?.logo?.description }/>
     </Link>
   );
 }
