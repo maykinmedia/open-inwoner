be28b1183d8eeb0c3d0df478bc0c9ff2735d4e7d
Anna Shamray <47745906+annashamray@users.noreply.github.com>
2021-11-24 11:33:48 +0100

    Feature/facets empty buckets (#28)
    * :sparkles: add empty facet values for every search call
    
    * :recycle: refactor search results - make them more OOPish
    
    * :green_heart: remove unused import to please the linter
    
    * :bug: show more than 10 results from search

--- src/open_inwoner/search/tests/test_search_products.py
+++ src/open_inwoner/search/tests/test_search_products.py
@@ -8,6 +8,7 @@ from open_inwoner.pdc.tests.factories import (
 )
 from open_inwoner.search.constants import FacetChoices
 
+from ..results import FacetBucket
 from ..searches import search_products
 from .utils import ESMixin
 
@@ -25,19 +26,19 @@ class SearchQueryTests(ESMixin, TestCase):
         self.update_index()
 
     def test_search_product_on_title(self):
-        results = search_products("Name").hits
+        results = search_products("Name").results
 
         self.assertEqual(len(results), 1)
         self.assertEqual(int(results[0].meta.id), self.product1.id)
 
     def test_search_product_on_summary(self):
-        results = search_products("summary").hits
+        results = search_products("summary").results
 
         self.assertEqual(len(results), 1)
         self.assertEqual(int(results[0].meta.id), self.product1.id)
 
     def test_search_product_on_content(self):
-        results = search_products("content").hits
+        results = search_products("content").results
 
         self.assertEqual(len(results), 1)
         self.assertEqual(int(results[0].meta.id), self.product1.id)
@@ -70,48 +71,92 @@ class SearchFacetTests(ESMixin, TestCase):
         self.update_index()
 
     def test_facets_top_level(self):
-        results = search_products("")
+        result = search_products("")
 
-        self.assertEqual(len(results.hits), 2)
+        self.assertEqual(len(result.results), 2)
 
-        facets = results.facets.to_dict()
-
+        facets = result.facets
+        self.assertEqual(len(facets), 3)
+        facet_categories, facet_tags, facet_orgs = facets
+        self.assertEqual(facet_categories.name, FacetChoices.categories)
         self.assertEqual(
-            facets,
-            {
-                FacetChoices.categories: [(self.category.name, 2, False)],
-                FacetChoices.tags: [
-                    (self.tag1.slug, 1, False),
-                    (self.tag2.slug, 1, False),
-                ],
-                FacetChoices.organizations: [
-                    (self.org1.slug, 1, False),
-                    (self.org2.slug, 1, False),
-                ],
-            },
+            facet_categories.buckets,
+            [
+                FacetBucket(
+                    slug=self.category.slug,
+                    name=self.category.name,
+                    count=2,
+                    selected=False,
+                )
+            ],
+        )
+        self.assertEqual(facet_tags.name, FacetChoices.tags)
+        self.assertEqual(
+            facet_tags.buckets,
+            [
+                FacetBucket(
+                    slug=self.tag1.slug, name=self.tag1.name, count=1, selected=False
+                ),
+                FacetBucket(
+                    slug=self.tag2.slug, name=self.tag2.name, count=1, selected=False
+                ),
+            ],
+        )
+        self.assertEqual(facet_orgs.name, FacetChoices.organizations)
+        self.assertEqual(
+            facet_orgs.buckets,
+            [
+                FacetBucket(
+                    slug=self.org1.slug, name=self.org1.name, count=1, selected=False
+                ),
+                FacetBucket(
+                    slug=self.org2.slug, name=self.org2.name, count=1, selected=False
+                ),
+            ],
         )
 
     def test_facets_with_filter(self):
-        results = search_products("", filters={"tags": self.tag1.slug})
+        result = search_products("", filters={"tags": self.tag1.slug})
 
-        self.assertEqual(len(results.hits), 1)
-        self.assertEqual(int(results[0].meta.id), self.product1.id)
+        self.assertEqual(len(result.results), 1)
+        self.assertEqual(int(result.results[0].meta.id), self.product1.id)
 
-        facets = results.facets.to_dict()
+        facets = result.facets
+        self.assertEqual(len(facets), 3)
+
+        facet_categories, facet_tags, facet_orgs = facets
+        self.assertEqual(facet_categories.name, FacetChoices.categories)
         self.assertEqual(
-            facets,
-            {
-                FacetChoices.categories: [(self.category.name, 1, False)],
-                FacetChoices.tags: [
-                    (self.tag1.slug, 1, True),
-                ],
-                FacetChoices.organizations: [
-                    (self.org1.slug, 1, False),
-                ],
-            },
+            facet_categories.buckets,
+            [
+                FacetBucket(
+                    slug=self.category.slug,
+                    name=self.category.name,
+                    count=1,
+                    selected=False,
+                )
+            ],
+        )
+        self.assertEqual(facet_tags.name, FacetChoices.tags)
+        self.assertEqual(
+            facet_tags.buckets,
+            [
+                FacetBucket(
+                    slug=self.tag1.slug, name=self.tag1.name, count=1, selected=True
+                )
+            ],
+        )
+        self.assertEqual(facet_orgs.name, FacetChoices.organizations)
+        self.assertEqual(
+            facet_orgs.buckets,
+            [
+                FacetBucket(
+                    slug=self.org1.slug, name=self.org1.name, count=1, selected=False
+                )
+            ],
         )
 
     def test_search_with_facet_filter(self):
-        results = search_products("other", filters={"tags": self.tag1.slug})
+        result = search_products("other", filters={"tags": self.tag1.slug})
 
-        self.assertEqual(len(results.hits), 0)
+        self.assertEqual(len(result.results), 0)
