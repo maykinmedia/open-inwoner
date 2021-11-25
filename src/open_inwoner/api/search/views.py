0862d7147d63a292cbcc4f1ec479c47fb5944b6f
Anna Shamray <anna.shamray@maykinmedia.nl>
2021-11-25 18:05:06 +0100

    :sparkles: add API endpoint for autocomplete

--- src/open_inwoner/api/search/views.py
+++ src/open_inwoner/api/search/views.py
@@ -1,12 +1,19 @@
 from drf_spectacular.utils import extend_schema
 from rest_framework.generics import GenericAPIView
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from open_inwoner.search.results import ProductSearchResult
-from open_inwoner.search.searches import search_products
+from open_inwoner.search.searches import search_autocomplete, search_products
 from open_inwoner.utils.schema import input_serializer_to_parameters
 
 from .pagination import SearchPagination
-from .serializers import SearchQuerySerializer, SearchResponseSerializer
+from .serializers import (
+    AutocompleteQuerySerializer,
+    AutocompleteResponseSerializer,
+    SearchQuerySerializer,
+    SearchResponseSerializer,
+)
 
 
 class SearchView(GenericAPIView):
@@ -21,7 +28,7 @@ class SearchView(GenericAPIView):
         """Search products by query string"""
         # validate query params
         query_serializer = SearchQuerySerializer(data=request.query_params)
-        query_serializer.is_valid()
+        query_serializer.is_valid(raise_exception=True)
         query_data = query_serializer.data
 
         # perform search
@@ -33,3 +40,23 @@ class SearchView(GenericAPIView):
             ProductSearchResult(results=page, facets=search_response.facets)
         )
         return self.get_paginated_response(serializer.data)
+
+
+class AutocompleteView(APIView):
+    permission_classes = []
+    authentication_classes = []
+    serializer_class = AutocompleteResponseSerializer
+
+    @extend_schema(
+        parameters=input_serializer_to_parameters(AutocompleteQuerySerializer)
+    )
+    def get(self, request, *args, **kwargs):
+        # validate query params
+        query_serializer = AutocompleteQuerySerializer(data=request.query_params)
+        query_serializer.is_valid(raise_exception=True)
+
+        # perform search
+        search_string = query_serializer.data["search"]
+        search_response = search_autocomplete(search_string)
+        serializer = self.serializer_class(search_response)
+        return Response(serializer.data)
