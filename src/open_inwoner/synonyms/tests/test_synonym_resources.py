50ac2cf082f1373123c566412e9ef5b1b1a9382b
Vasileios Zigras <zigras00@gmail.com>
2021-11-30 09:55:45 +0100

    [#108] Added tests

---
+++ src/open_inwoner/synonyms/tests/test_synonym_resources.py
@@ -0,0 +1,69 @@
+from collections import OrderedDict
+
+from django.core.exceptions import ValidationError
+from django.test import TestCase
+
+import tablib
+
+from ..models import Synonym
+from ..resources import SynonymExportResource, SynonymImportResource
+from .factories import SynonymFactory
+
+
+class TestSynonymImportResource(TestCase):
+    def setUp(self):
+        self.synonym = SynonymFactory.create()
+        self.resource = SynonymImportResource()
+
+    def test_import_saves_term_and_synonyms(self):
+        dataset = tablib.Dataset(
+            [self.synonym.term, self.synonym.synonyms],
+            headers=[
+                "Term",
+                "UF",
+            ],
+        )
+        self.resource.import_data(dataset)
+        qs = Synonym.objects.filter(term=self.synonym.term)
+
+        self.assertEqual(qs.count(), 1)
+
+    def test_import_raises_validation_error_with_wrong_headers(self):
+        dataset = tablib.Dataset(
+            [self.synonym.term, self.synonym.synonyms],
+            headers=[
+                "Wrong header",
+                "Another wrong header",
+            ],
+        )
+        with self.assertRaises(ValidationError) as e:
+            self.resource.import_data(dataset, raise_errors=True)
+
+        self.assertEqual(
+            e.exception.message_dict,
+            {
+                "term": ["Dit veld kan niet leeg zijn"],
+                "synonyms": ["Dit veld mag niet leeg zijn."],
+            },
+        )
+
+
+class TestSynonymExportResource(TestCase):
+    def setUp(self):
+        self.synonym = SynonymFactory()
+        self.resource = SynonymExportResource()
+
+    def test_export_returns_expected_row(self):
+        dataset = self.resource.export()
+        self.maxDiff = None
+        self.assertEqual(
+            dataset.dict,
+            [
+                OrderedDict(
+                    [
+                        (dataset.headers[0], self.synonym.term),
+                        (dataset.headers[1], ",".join(self.synonym.synonyms)),
+                    ]
+                ),
+            ],
+        )
