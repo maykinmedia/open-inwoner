411909a7f250a3ff5783f1ea4b9c8ff586bf7942
Alex de Landgraaf <alex@maykinmedia.nl>
2021-12-07 22:32:42 +0100

    Fixed zaanstad -- Redis only goes to 15 Ensuring deployment of staging doesn't end with an error due to post-deploy status check

--- src/open_inwoner/conf/staging_zaanstad.py
+++ src/open_inwoner/conf/staging_zaanstad.py
@@ -6,7 +6,7 @@ import os
 
 os.environ.setdefault("ENVIRONMENT", "staging")
 # NOTE: watch out for multiple projects using the same cache!
-os.environ.setdefault("CACHE_DEFAULT", "127.0.0.1:6379/16")
+os.environ.setdefault("CACHE_DEFAULT", "127.0.0.1:6379/10")
 
 os.environ['DB_NAME'] = 'oip-staging-zaanstad'
 os.environ['ALLOWED_HOSTS'] = 'zaanstad.openinwoner.nl'
