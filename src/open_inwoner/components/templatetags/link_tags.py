02b7580dfd8856bfdd11b015514ccf234ad2544a
Sven van de Scheur <svenvandescheur@gmail.com>
2021-12-10 11:40:49 +0100

    :ok_hand: - Please add all the options that someone can pass through. So that we don't have to scan the template.

--- src/open_inwoner/components/templatetags/link_tags.py
+++ src/open_inwoner/components/templatetags/link_tags.py
@@ -6,6 +6,19 @@ register = template.Library()
 
 @register.inclusion_tag("components/Typography/Link.html")
 def link(href, **kwargs):
+    """
+    href: url | where the link links to.
+
+    text: string | this will be the link text. (Optional)
+    download: bool | whether the link should be downloaded instead of linked to. (Optional)
+    extra_classes: string | additional CSS classes.
+    icon_position: "before" or "after" | where the icon should be positioned to the text. (Optional)
+    primary: bool | if the primary colors should be used. (Optional)
+    secondary: bool | if the secondary colors should be used. (Optional)
+    icon: string | the icon that you want to display. (Optional)
+    social_icon: string | the social icon that you want to display. (Optional)
+    type: string | the type of button that should be used. (Optional)
+    """
     try:
         href = reverse(href)
     except NoReverseMatch:
