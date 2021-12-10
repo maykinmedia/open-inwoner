81e6ffc72c3a4962a1e00ae3a44b38c09e97c2eb
Sven van de Scheur <svenvandescheur@gmail.com>
2021-12-10 11:27:44 +0100

    :ok_hand: - Please add all the options that someone can pass through. So that we don't have to scan the template.

--- src/open_inwoner/components/templatetags/card_tags.py
+++ src/open_inwoner/components/templatetags/card_tags.py
@@ -8,11 +8,24 @@ register = template.Library()
 
 @register.inclusion_tag("components/Card/Card.html")
 def card(href, title, **kwargs):
+    """
+    href: url | where the card links to. (Optional)
+    title: string | this will be the card title. (Optional)
+
+    alt: string | the alt of the header image. (optional)
+    direction: string | can be set to "horizontal" to show contents horizontally (Optional).
+    src: string | the src of the header image. (optional)
+    tinted: bool | whether to use gray as background color. (Optional)
+    """
     return {**kwargs, "href": href, "title": title}
 
 
 @register.tag
 def render_card(parser, token):
+    """
+    Supports all options from card.
+    Nested content supported.
+    """
     bits = token.split_contents()
     context_kwargs = parse_component_with_args(parser, bits, "render_card")
     nodelist = parser.parse(("endrender_card",))
@@ -22,16 +35,30 @@ def render_card(parser, token):
 
 @register.inclusion_tag("components/Card/CategoryCard.html")
 def category_card(title, products, **kwargs):
+    """
+    title: string | this will be the card title. (Optional)
+    products: Product[] | products to render.
+    href: url | where the card links to. (Optional)
+    """
     return {**kwargs, "products": products, "title": title}
 
 
 @register.inclusion_tag("components/Card/ProductCard.html")
 def product_card(title, description, href, **kwargs):
+    """
+    title: string | this will be the card title. (Optional)
+    description: string | this will be the card description. (Optional)
+    """
     return {**kwargs, "description": description, "title": title, "href": href}
 
 
 @register.inclusion_tag("components/Card/CardContainer.html")
 def card_container(categories=[], subcategories=[], products=[], **kwargs):
+    """
+    categories: Category[] | categories to render.
+    subcategories: Category[] | subcategories to render.
+    products: Product[] | products to render.
+    """
     if categories is None and subcategories is None and products is None:
         assert False, "provide categories, subcategories or products"
     return {
