cfc32dee31349e672e0707f0f76547eee5b97ef7
Sven van de Scheur <svenvandescheur@gmail.com>
2021-12-13 10:25:17 +0100

    Issue/#121 (#45)
    * :sparkles: #121 - Login page.
    
    * :see_no_evil: - Update .gitignore.
    
    * :sparkles: - Login page improvements.
    
    * :shirt: - black.
    
    * :shirt: - isort.
    
    * :ok_hand: - Please add all the options that someone can pass through. So that we don't have to scan the template.
    
    * :ok_hand: - I think that it will be better to split the cards. Since they now serve multiple purposes and are getting complicated.
    
    * :ok_hand: - Why is the slash added?
    
    * :ok_hand: - Please add all the options that someone can pass through. So that we don't have to scan the template.
    
    * :ok_hand: - Please add all the options that someone can pass through. So that we don't have to scan the template.
    
    * :ok_hand: - If we are going to support reverse you should be able to also pass the reverse kwargs. Otherwise, please revert.
    
    * :ok_hand: - Also provide a way to add the hero image to a page without overwriting main_outer.
    
    * :ok_hand: - Please reset this. Since this will break other pages.
    
    * :shirt: - Black.
    
    * :lipstick: - Redesigned login page.
    
    * :shirt: - Black.
    
    * :lipstick: - Make DigiD login bold.

---
+++ src/open_inwoner/components/templatetags/grid_tags.py
@@ -0,0 +1,36 @@
+from django import template
+
+from open_inwoner.components.templatetags.form_tags import parse_component_with_args
+from open_inwoner.utils.templatetags.abstract import ContentsNode
+
+register = template.Library()
+
+
+@register.tag
+def render_grid(parser, token):
+    """
+    Nested content supported.
+    """
+    bits = token.split_contents()
+    context_kwargs = parse_component_with_args(parser, bits, "render_grid")
+    nodelist = parser.parse(("endrender_grid",))
+    parser.delete_first_token()
+    return ContentsNode(nodelist, "components/Grid/Grid.html", **context_kwargs)
+
+
+@register.tag
+def render_column(parser, token):
+    """
+    start: int | column to start from.
+    span: int | column span.
+    Nested content supported.
+    """
+    bits = token.split_contents()
+    context_kwargs = parse_component_with_args(parser, bits, "render_grid")
+    nodelist = parser.parse(("endrender_column",))
+    parser.delete_first_token()
+
+    context_kwargs["start"] = context_kwargs.get("start", 1)
+    context_kwargs["span"] = context_kwargs.get("span", 12)
+
+    return ContentsNode(nodelist, "components/Grid/Column.html", **context_kwargs)
--- src/open_inwoner/components/templatetags/link_tags.py
+++ src/open_inwoner/components/templatetags/link_tags.py
@@ -1,8 +1,32 @@
 from django import template
+from django.urls import NoReverseMatch, reverse
 
 register = template.Library()
 
 
 @register.inclusion_tag("components/Typography/Link.html")
 def link(href, **kwargs):
-    return {**kwargs, "href": href}
+    """
+    href: url | where the link links to (can be url name).
+
+    bold: bool | whether the link should be bold.
+    download: bool | whether the link should be downloaded instead of linked to. (Optional)
+    extra_classes: string | additional CSS classes.
+    icon: string | the icon that you want to display. (Optional)
+    icon_position: "before" or "after" | where the icon should be positioned to the text. (Optional)
+    primary: bool | if the primary colors should be used. (Optional)
+    reverse_kwargs: dict | if href is an url name, kwargs for reverse can be passed (Optional).
+    secondary: bool | if the secondary colors should be used. (Optional)
+    social_icon: string | the social icon that you want to display. (Optional)
+    text: string | this will be the link text. (Optional)
+    type: string | the type of button that should be used. (Optional)
+    """
+    try:
+        reverse_kwargs = kwargs.get("reverse_kwargs")
+        href = reverse(href, kwargs=reverse_kwargs)
+    except NoReverseMatch:
+        pass
+
+    kwargs["href"] = href
+
+    return kwargs
