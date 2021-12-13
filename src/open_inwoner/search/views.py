ee2130ffd69fc22eaef3843ace391dc6f14cc50c
Anna Shamray <anna.shamray@maykinmedia.nl>
2021-12-13 13:00:34 +0100

    :sparkles: add facets into the page form

--- src/open_inwoner/search/views.py
+++ src/open_inwoner/search/views.py
@@ -9,10 +9,17 @@ class SearchView(FormView):
     form_class = SearchForm
     template_name = "pages/search.html"
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        if self.request.GET.get("query"):
-            context["results"] = search_products(
-                self.request.GET.get("query"),
-            )
-        return context
+    def form_valid(self, form):
+        data = form.cleaned_data.copy()
+        query = data.pop("query")
+
+        # perform search
+        results = search_products(query, filters=data)
+
+        # update form fields with choices
+        for facet in results.facets:
+            if facet.name in form.fields:
+                form.fields[facet.name].choices = facet.choices()
+
+        context = self.get_context_data(form=form, results=results)
+        return self.render_to_response(context)
