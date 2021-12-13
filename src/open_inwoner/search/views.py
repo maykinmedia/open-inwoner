0c52ff77994bd88df31740f9ef4439b73d298763
Anna Shamray <anna.shamray@maykinmedia.nl>
2021-12-13 17:53:47 +0100

    :recycle: bring back GET method for the form

--- src/open_inwoner/search/views.py
+++ src/open_inwoner/search/views.py
@@ -9,9 +9,28 @@ class SearchView(FormView):
     form_class = SearchForm
     template_name = "pages/search.html"
 
+    def get(self, request, *args, **kwargs):
+        form = self.get_form()
+        if form.is_valid():
+            return self.form_valid(form)
+        else:
+            return self.form_invalid(form)
+
+    def get_form_kwargs(self):
+        """Return the keyword arguments for instantiating the form."""
+        kwargs = {
+            "initial": self.get_initial(),
+            "prefix": self.get_prefix(),
+            "data": self.request.GET,
+        }
+        return kwargs
+
     def form_valid(self, form):
         data = form.cleaned_data.copy()
         query = data.pop("query")
+
+        if not query:
+            return self.render_to_response(self.get_context_data(form=form))
 
         # perform search
         results = search_products(query, filters=data)
