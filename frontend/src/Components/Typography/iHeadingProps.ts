157cc1590a5ce5affc4671c499fb44515d0f835e
Jorik Kraaikamp <jorikkraaikamp@gmail.com>
2021-12-14 16:36:22 +0100

    Deleted frontend

--- frontend/src/Components/Typography/iHeadingProps.ts
+++
@@ -1,6 +0,0 @@
-export interface iHeadingProps {
-  autoId?: boolean
-  id?: string,
-  children?: any,
-  className?: string,
-}
--- frontend/src/types/attr.ts
+++
@@ -1,4 +0,0 @@
-export interface iAttr {
-  name: string,
-  value: any,
-}
