c4153a927f7f914bc2136c56442a8f15aeeaf629
Anna Shamray <anna.shamray@maykinmedia.nl>
2021-12-16 11:44:21 +0100

    :sparkles: group messages by date

--- src/open_inwoner/accounts/query.py
+++ src/open_inwoner/accounts/query.py
@@ -1,10 +1,23 @@
+from collections import OrderedDict
+from datetime import date
 from itertools import groupby
+from typing import Dict
 
 from django.db.models import Q, query
 
 
 class MessageQuerySet(query.QuerySet):
-    def get_messages_between_users(self, me, other_user):
-        return self.filter(
-            Q(sender=me, receiver=other_user) | Q(sender=other_user, receiver=me)
-        ).order_by("-created_on")
+    def get_messages_between_users(self, me, other_user) -> Dict[date, list]:
+        """grouped by date"""
+        messages = list(
+            self.filter(
+                Q(sender=me, receiver=other_user) | Q(sender=other_user, receiver=me)
+            ).order_by("created_on")
+        )
+
+        groups = OrderedDict()
+
+        for create_date, group in groupby(messages, lambda x: x.created_on.date()):
+            groups[create_date] = list(group)
+
+        return groups
