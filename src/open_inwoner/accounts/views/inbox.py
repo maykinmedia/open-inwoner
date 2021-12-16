78f8aadc71044848fae5806162d749d8c7144540
Anna Shamray <anna.shamray@maykinmedia.nl>
2021-12-16 16:05:56 +0100

    :sparkles: add form submit

--- src/open_inwoner/accounts/views/inbox.py
+++ src/open_inwoner/accounts/views/inbox.py
@@ -1,15 +1,32 @@
 from django import forms
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404
 from django.views.generic import FormView
+
+from furl import furl
 
 from open_inwoner.utils.mixins import PaginationMixin
 
 from ..models import Message, User
 
 
-class InboxForm(forms.Form):
-    message = forms.CharField(label="", widget=forms.Textarea)
+class InboxForm(forms.ModelForm):
+    content = forms.CharField(label="", widget=forms.Textarea)
+    receiver = forms.ModelChoiceField(
+        queryset=User.objects.all(),
+        to_field_name="email",
+        widget=forms.HiddenInput(),
+    )
+
+    class Meta:
+        model = Message
+        fields = ("content", "receiver")
+
+    def save(self, sender=None, commit=True):
+        self.instance.sender = sender
+
+        return super().save(commit)
 
 
 class InboxView(LoginRequiredMixin, PaginationMixin, FormView):
@@ -52,3 +69,20 @@ class InboxView(LoginRequiredMixin, PaginationMixin, FormView):
         )
 
         return context
+
+    def get_initial(self):
+        initial = super().get_initial()
+
+        message_user_email = self.request.GET.get("with")
+        if message_user_email:
+            initial["receiver"] = message_user_email
+
+        return initial
+
+    def form_valid(self, form):
+        form.save(sender=self.request.user)
+
+        # build redirect url based on form hidden data
+        url = furl(self.request.path).add({"with": form.data["receiver"]}).url
+
+        return HttpResponseRedirect(url)
