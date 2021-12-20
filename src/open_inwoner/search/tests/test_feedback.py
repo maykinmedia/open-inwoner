810d3ef88b64df8c11aeda54211937260d793d64
Vasileios Zigras <zigras00@gmail.com>
2021-12-20 10:25:34 +0100

    [#193] Added tests for feedback functionality, updated tests for search page

---
+++ src/open_inwoner/search/tests/test_feedback.py
@@ -0,0 +1,104 @@
+import urllib
+
+from django.urls import reverse
+
+from django_webtest import WebTest
+
+from open_inwoner.accounts.tests.factories import UserFactory
+from open_inwoner.pdc.tests.factories import CategoryFactory, TagFactory
+
+from ..models import Feedback
+from .factories import FeedbackFactory
+from .utils import ESMixin
+
+
+class TestFeedbackFunctionality(ESMixin, WebTest):
+    def setUp(self):
+        self.user = UserFactory()
+        self.feedback = FeedbackFactory.build()
+        self.category1 = CategoryFactory.build()
+        self.category2 = CategoryFactory.build()
+        self.tag1 = TagFactory.build()
+        self.tag2 = TagFactory.build()
+        self.update_index()
+
+    def test_feedback_is_saved_with_authenticated_user_and_without_filters(self):
+        self.app.set_user(user=self.user)
+        params = {"query": self.feedback.search_query}
+        url = f"{reverse('search:search')}?{urllib.parse.urlencode(params, doseq=True)}"
+        response = self.app.get(url)
+
+        feedback_form = response.forms[1]
+        feedback_form["remark"] = self.feedback.remark
+        feedback_form["positive"] = self.feedback.positive
+        feedback_form.submit()
+
+        feedback = Feedback.objects.all()[0]
+
+        self.assertEqual(feedback.search_query, f"query: {self.feedback.search_query}")
+        self.assertEqual(feedback.search_url, url)
+        self.assertEqual(feedback.positive, self.feedback.positive)
+        self.assertEqual(feedback.remark, self.feedback.remark)
+        self.assertEqual(feedback.searched_by, self.user)
+
+    def test_feedback_is_saved_with_unauthenticated_user_and_without_filters(self):
+        params = {"query": self.feedback.search_query}
+        url = f"{reverse('search:search')}?{urllib.parse.urlencode(params, doseq=True)}"
+        response = self.app.get(url)
+
+        feedback_form = response.forms[1]
+        feedback_form["remark"] = self.feedback.remark
+        feedback_form["positive"] = self.feedback.positive
+        feedback_form.submit()
+
+        feedback = Feedback.objects.all()[0]
+
+        self.assertEqual(feedback.search_query, f"query: {self.feedback.search_query}")
+        self.assertEqual(feedback.search_url, url)
+        self.assertEqual(feedback.positive, self.feedback.positive)
+        self.assertEqual(feedback.remark, self.feedback.remark)
+        self.assertIsNone(feedback.searched_by)
+
+    def test_feedback_is_saved_with_authenticated_user_and_with_filters(self):
+        self.app.set_user(user=self.user)
+        params = {
+            "query": [self.feedback.search_query],
+            "categories": [self.category1.name, self.category2.name],
+            "tags": [self.tag1.name, self.tag2.name],
+        }
+        url = f"{reverse('search:search')}?{urllib.parse.urlencode(params, doseq=True)}"
+        response = self.app.get(url)
+
+        feedback_form = response.forms[1]
+        feedback_form["remark"] = self.feedback.remark
+        feedback_form["positive"] = self.feedback.positive
+        feedback_form.submit()
+
+        feedback = Feedback.objects.all()[0]
+
+        self.assertEqual(
+            feedback.search_query,
+            f"query: {self.feedback.search_query} | categories: {self.category1.name}, {self.category2.name} | tags: {self.tag1.name}, {self.tag2.name}",
+        )
+        self.assertEqual(feedback.search_url, url)
+        self.assertEqual(feedback.positive, self.feedback.positive)
+        self.assertEqual(feedback.remark, self.feedback.remark)
+        self.assertEqual(feedback.searched_by, self.user)
+
+    def test_feedback_is_saved_with_unauthenticated_user_and_with_filters(self):
+        params = {"query": self.feedback.search_query}
+        url = f"{reverse('search:search')}?{urllib.parse.urlencode(params, doseq=True)}"
+        response = self.app.get(url)
+
+        feedback_form = response.forms[1]
+        feedback_form["remark"] = self.feedback.remark
+        feedback_form["positive"] = self.feedback.positive
+        feedback_form.submit()
+
+        feedback = Feedback.objects.all()[0]
+
+        self.assertEqual(feedback.search_query, f"query: {self.feedback.search_query}")
+        self.assertEqual(feedback.search_url, url)
+        self.assertEqual(feedback.positive, self.feedback.positive)
+        self.assertEqual(feedback.remark, self.feedback.remark)
+        self.assertIsNone(feedback.searched_by)
