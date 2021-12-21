38bbb8d3c49b06c8cb535ec43e3b74ee6956195c
Jorik Kraaikamp <jorikkraaikamp@gmail.com>
2021-12-21 11:31:16 +0100

    Fixed frontend linting

--- src/open_inwoner/js/components/datepicker/index.js
+++ src/open_inwoner/js/components/datepicker/index.js
@@ -1,8 +1,8 @@
-import flatpickr from "flatpickr";
-import { Dutch } from "flatpickr/dist/l10n/nl"
+import flatpickr from 'flatpickr'
+import { Dutch } from 'flatpickr/dist/l10n/nl'
 
-flatpickr(".datefield", {
-    weekNumbers: true,
-    dateFormat: "d-m-Y",
-    locale: Dutch
-});
+flatpickr('.datefield', {
+  weekNumbers: true,
+  dateFormat: 'd-m-Y',
+  locale: Dutch,
+})
