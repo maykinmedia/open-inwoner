0cb2d4d55541da8c1875896c861e783d336547d3
Jorik Kraaikamp <jorikkraaikamp@gmail.com>
2021-12-21 15:54:22 +0100

    Fixed frontend linting

--- src/open_inwoner/js/components/messages/index.js
+++ src/open_inwoner/js/components/messages/index.js
@@ -1,14 +1,14 @@
 class CloseMessages {
-    constructor(close) {
-        this.close = close;
-        this.close.addEventListener('click', this.closeMessage.bind(this));
-    }
+  constructor(close) {
+    this.close = close
+    this.close.addEventListener('click', this.closeMessage.bind(this))
+  }
 
-    closeMessage(event) {
-        event.preventDefault();
-        this.close.parentElement.remove();
-    }
+  closeMessage(event) {
+    event.preventDefault()
+    this.close.parentElement.remove()
+  }
 }
 
-const closeButtons = document.querySelectorAll('.message__close');
-[...closeButtons].forEach((closeButton) => new CloseMessages(closeButton));
+const closeButtons = document.querySelectorAll('.message__close')
+;[...closeButtons].forEach((closeButton) => new CloseMessages(closeButton))
