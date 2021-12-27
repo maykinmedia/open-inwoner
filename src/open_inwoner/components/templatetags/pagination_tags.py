7b8bc76a310d07a234a87ebd597a9240bfd1c74e
Anna Shamray <anna.shamray@maykinmedia.nl>
2021-12-27 10:39:13 +0100

    :sparkles: add lookaround variable to pagination

--- src/open_inwoner/components/templatetags/pagination_tags.py
+++ src/open_inwoner/components/templatetags/pagination_tags.py
@@ -4,17 +4,25 @@ register = template.Library()
 
 
 @register.inclusion_tag("components/Pagination/Pagination.html")
-def pagination(page_obj, paginator, request, **kwargs):
+def pagination(page_obj, paginator, request, lookaround=3, **kwargs):
     """
     page_obj: Default django page object
     paginator: Default django paginator
     request: The django request (this will be for the querystring tag. So it is not used yet)
+    lookaround: int | The amount of pages to render around the active page number (default=3)
 
     {% pagination page_obj=page_obj paginator=paginator request=request %}
     """
+    page_numbers = [
+        page_obj.number - lookaround + i
+        for i in range(lookaround * 2 + 1)
+        if (1 < page_obj.number - lookaround + i < paginator.num_pages)
+    ]
+
     return {
         "page_obj": page_obj,
         "paginator": paginator,
         "request": request,
+        "page_numbers": page_numbers,
         **kwargs,
     }
