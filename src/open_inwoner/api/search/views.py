5227bcce3db3e090e6512eaf1f0338c4b0d54f91
Anna Shamray <anna.shamray@maykinmedia.nl>
2021-12-27 16:54:05 +0100

    :fire: remove unused search api

--- src/open_inwoner/api/search/views.py
+++ src/open_inwoner/api/search/views.py
@@ -1,45 +1,11 @@
 from drf_spectacular.utils import extend_schema
-from rest_framework.generics import GenericAPIView
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from open_inwoner.search.results import ProductSearchResult
-from open_inwoner.search.searches import search_autocomplete, search_products
+from open_inwoner.search.searches import search_autocomplete
 from open_inwoner.utils.schema import input_serializer_to_parameters
 
-from .pagination import SearchPagination
-from .serializers import (
-    AutocompleteQuerySerializer,
-    AutocompleteResponseSerializer,
-    SearchQuerySerializer,
-    SearchResponseSerializer,
-)
-
-
-class SearchView(GenericAPIView):
-    permission_classes = []
-    authentication_classes = []
-    serializer_class = SearchResponseSerializer
-    pagination_class = SearchPagination
-    action = "list"  # for spectacular
-
-    @extend_schema(parameters=input_serializer_to_parameters(SearchQuerySerializer))
-    def get(self, request, *args, **kwargs):
-        """Search products by query string"""
-        # validate query params
-        query_serializer = SearchQuerySerializer(data=request.query_params)
-        query_serializer.is_valid(raise_exception=True)
-        query_data = query_serializer.data
-
-        # perform search
-        search_string = query_data.pop("search", "")
-        search_response = search_products(search_string, filters=query_data)
-        # paginate
-        page = self.paginate_queryset(search_response.results)
-        serializer = self.get_serializer(
-            ProductSearchResult(results=page, facets=search_response.facets)
-        )
-        return self.get_paginated_response(serializer.data)
+from .serializers import AutocompleteQuerySerializer, AutocompleteResponseSerializer
 
 
 class AutocompleteView(APIView):
