ea80f451facb6d72ce155348ffafb0cedc028dc2
Jorik Kraaikamp <jorikkraaikamp@gmail.com>
2022-01-03 12:33:02 +0100

    Added better button docstrings

--- src/open_inwoner/components/templatetags/button_tags.py
+++ src/open_inwoner/components/templatetags/button_tags.py
@@ -10,7 +10,20 @@ register = template.Library()
 @register.tag
 def button_row(parser, token):
     """
-    Nested content supported.
+    Creating a row of buttons. This expects buttons inside of the component.
+    This is only a wrapper to make sure that the buttons are displayed correctly.
+
+    Usage:
+    {% button_row %}
+        {% button text="button 1" %}
+        {% button text="button 2" %}
+    {% endbutton_row %}
+
+    Variables:
+    - align: enum[right] | if the buttons should be aligned left (no align should be given) or alinged right side.
+
+    Extra context:
+    - contents: string (HTML) | this is the context between the button_row and endbutton_row tags
     """
     bits = token.split_contents()
     context_kwargs = parse_component_with_args(parser, bits, "button_row")
@@ -22,21 +35,27 @@ def button_row(parser, token):
 @register.inclusion_tag("components/Button/Button.html")
 def button(**kwargs):
     """
-    text: string | this will be the button text. (Optional)
-    hide_text: bool | whether to hide the text and use aria attribute instead. (Optional).
-    href: url | where the link links to (can be url name). (Optional)
-    object_id: str | if href is an url name, object_id for reverse can be passed (Optional).
-    uuid: str | if href is an url name, uuid for reverse can be passed (Optional).
-    size: "big" | If the button should be bigger. (Optional)
-    open: bool | If the open style button should be used. (Optional)
-    bordered: bool | If the border should be colored. (Optional)
-    primary: bool | If the primary colors should be used. (Optional)
-    secondary: bool | If the secondary colors should be used. (Optional)
-    transparent: bool | If the button does not have a background or border. (Optional)
-    icon: string | the icon that you want to display. (Optional)
-    icon_position: "before" or "after" | where the icon should be positioned to the text. (Optional)
-    icon_outlined: bool | if the outlined icons should be used. (Optional)
-    type: string | the type of button that should be used. (Optional)
+    Creating a button. This can be a HTML button or an anchor element.
+
+    Usage:
+    {% button text="Button" icon="arrow-forward" primary=True %}
+
+    Variables:
+    - text: string | this will be the button text. (Optional)
+    - hide_text: bool | whether to hide the text and use aria attribute instead. (Optional).
+    - href: url or string | where the link links to (can be url name). (Optional)
+    - object_id: str | if href is an url name, object_id for reverse can be passed (Optional).
+    - uuid: string | if href is an url name, pk for reverse can be passed (Optional).
+    - size: enum[big] | If the button should be bigger. (Optional)
+    - open: bool | If the open style button should be used. (Optional)
+    - bordered: bool | If the border should be colored. (Optional)
+    - primary: bool | If the primary colors should be used. (Optional)
+    - secondary: bool | If the secondary colors should be used. (Optional)
+    - transparent: bool | If the button does not have a background or border. (Optional)
+    - icon: string | the icon that you want to display. (Optional)
+    - icon_position: enum[before, after] | where the icon should be positioned to the text. (Optional)
+    - icon_outlined: bool | if the outlined icons should be used. (Optional)
+    - type: string | the type of button that should be used. (Optional)
     """
     if "text" not in kwargs and "icon" not in kwargs:
         assert False, "Either text or icon should be given"
