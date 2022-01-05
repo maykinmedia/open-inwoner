ddf7ce5eec795d66606a81265ec736328090aad4
Jorik Kraaikamp <jorikkraaikamp@gmail.com>
2022-01-05 15:53:18 +0100

    added some extra tests to prove that the actions and contacts are scoped to a user

--- src/open_inwoner/accounts/tests/test_contact_views.py
+++ src/open_inwoner/accounts/tests/test_contact_views.py
@@ -2,25 +2,51 @@ from django.urls import reverse
 
 from django_webtest import WebTest
 
-from .factories import ContactFactory
+from .factories import ContactFactory, UserFactory
 
 
 class ContactViewTests(WebTest):
+    def setUp(self) -> None:
+        self.user = UserFactory()
+        self.contact = ContactFactory(
+            first_name="contact_that_should_be_found", created_by=self.user
+        )
+
+        self.login_url = reverse("login")
+        self.list_url = reverse("accounts:contact_list")
+        self.edit_url = reverse(
+            "accounts:contact_edit", kwargs={"uuid": self.contact.uuid}
+        )
+        self.create_url = reverse("accounts:contact_create")
+
     def test_contact_list_login_required(self):
-        login_url = reverse("login")
-        url = reverse("accounts:contact_list")
-        response = self.app.get(url)
-        self.assertRedirects(response, f"{login_url}?next={url}")
+        response = self.app.get(self.list_url)
+        self.assertRedirects(response, f"{self.login_url}?next={self.list_url}")
+
+    def test_contact_list_filled(self):
+        response = self.app.get(self.list_url, user=self.user)
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, self.contact.first_name)
+
+    def test_contact_list_only_show_personal_contacts(self):
+        other_user = UserFactory()
+        response = self.app.get(self.list_url, user=other_user)
+        self.assertEqual(response.status_code, 200)
+        self.assertNotContains(response, self.contact.first_name)
 
     def test_contact_edit_login_required(self):
-        contact = ContactFactory()
-        login_url = reverse("login")
-        url = reverse("accounts:contact_edit", kwargs={"uuid": contact.uuid})
-        response = self.app.get(url)
-        self.assertRedirects(response, f"{login_url}?next={url}")
+        response = self.app.get(self.edit_url)
+        self.assertRedirects(response, f"{self.login_url}?next={self.edit_url}")
+
+    def test_contact_edit(self):
+        response = self.app.get(self.edit_url, user=self.user)
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, self.contact.first_name)
+
+    def test_contact_edit_not_your_contact(self):
+        other_user = UserFactory()
+        response = self.app.get(self.edit_url, user=other_user, status=404)
 
     def test_contact_create_login_required(self):
-        login_url = reverse("login")
-        url = reverse("accounts:contact_create")
-        response = self.app.get(url)
-        self.assertRedirects(response, f"{login_url}?next={url}")
+        response = self.app.get(self.create_url)
+        self.assertRedirects(response, f"{self.login_url}?next={self.create_url}")
