e0ce18a43fa9646279a20b19bddfd5cc0c07a54a
Sven van de Scheur <svenvandescheur@gmail.com>
2022-01-05 15:30:52 +0100

    :sparkles: #237 #236 - show combined map of productlocations (#75)
    * :sparkles: #237 #236 - show combined map of productlocations
    
    * :green_heart: - Fix the build.
    
    * :shirt: - Run prettier.
    
    * :green_heart: - Attempt to fix the build.
    
    * :sparkles: #145 - Add Kadaster tiles in the frontend.
    
    * :ok_hand: - That is why the test is not needed. If the factory fails all other tests that use the factory will fail.
    
    * :shirt: - Run prettier.

--- src/open_inwoner/pdc/mixins.py
+++ src/open_inwoner/pdc/mixins.py
@@ -1,12 +1,59 @@
+import json
+import re
+from typing import Union
+
 from django.contrib.gis.db.models import PointField
 from django.core.exceptions import ValidationError
-from django.db import models
+from django.db import connection, models
 from django.utils.translation import ugettext_lazy as _
 
 from geopy.exc import GeopyError
 from localflavor.nl.models import NLZipCodeField
 
 from .geocode import geocode_address
+
+
+class GeoModelQuerySet(models.QuerySet):
+    def get_geojson_feature_collection(self) -> str:
+        """
+        Returns a geojson feature collection for all objects in this queryset.
+        """
+        return json.dumps(
+            {
+                "type": "FeatureCollection",
+                "features": [json.loads(o.get_geojson_feature()) for o in self],
+            }
+        )
+
+    def get_centroid(self) -> Union[dict, None]:
+        """
+        I apologize.
+
+        This uses postgis to figure out the centroid of the objects in this queryset.
+        """
+        ids = self.values_list("id", flat=True)
+
+        if not ids:
+            return
+
+        with connection.cursor() as cursor:
+            cursor.execute(
+                "SELECT ST_AsText(ST_Centroid(ST_Collect(geometry))) "
+                "FROM pdc_productlocation "
+                "WHERE id in %s",
+                [tuple(ids)],
+            )
+            row = cursor.fetchone()
+
+        try:
+            point = row[0]
+            m = re.findall(r"[0-9\.]+", point)
+            return {
+                "lng": m[0],
+                "lat": m[1],
+            }
+        except IndexError:
+            return
 
 
 class GeoModel(models.Model):
@@ -26,14 +73,24 @@ class GeoModel(models.Model):
         help_text=_("Geo coordinates of the location"),
     )
 
+    objects = GeoModelQuerySet.as_manager()
+
     class Meta:
         abstract = True
 
     @property
     def address_str(self):
+        return f"{self.address_line_1}, {self.address_line_2}"
+
+    @property
+    def address_line_1(self):
+        return f"{self.street} {self.housenumber}"
+
+    @property
+    def address_line_2(self):
         # geocoding doesn't work if postcode has space inside
         postcode = self.postcode.replace(" ", "")
-        return f"{self.street} {self.housenumber}, {postcode} {self.city}"
+        return f"{postcode} {self.city}"
 
     def clean(self):
         super().clean()
@@ -62,3 +119,42 @@ class GeoModel(models.Model):
                 )
             )
         self.geometry = geometry
+
+    def get_geojson_feature(self) -> str:
+        """
+        Returns a geojson feature for this object.
+        """
+        return json.dumps(
+            {
+                "type": "Feature",
+                "geometry": json.loads(self.get_geojson_geometry()),
+                "properties": self.get_serialized_fields(),
+            }
+        )
+
+    def get_geojson_geometry(self) -> str:
+        """
+        Returns a geojson geometry for this object.
+        """
+        return str(self.geometry.geojson)
+
+    def get_serialized_fields(self) -> dict:
+        """
+        Returns fields listed in serializable_fields (see below) as dict if JSON serializable.
+        """
+        serializable_fields = ["name", "street", "housenumber", "postcode", "city"]
+        serialized_fields = {}
+
+        for name in serializable_fields:
+            if not self._meta.get_field(name):
+                continue
+
+            value = getattr(self, name)
+
+            try:
+                json.dumps(value)  # Check if json serializable.
+                serialized_fields[name] = value
+            except TypeError:
+                continue
+
+        return serialized_fields
