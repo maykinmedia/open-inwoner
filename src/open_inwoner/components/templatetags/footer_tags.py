069b37631b8973cc6c26cc419437082becaed628
Vasileios Zigras <zigras00@gmail.com>
2022-01-17 09:21:08 +0100

    Feature/configurable fixed texts (#58)
    * [#192] Added configuration for home page texts
    
    * [#192] Removed social footer for now,added addresses wysiwyg field
    
    * [#192] Added flatpages for footer links
    
    * [#193] Added the rest of configurable texts and fixed order in flatpages
    
    * [#192] Updated footer template and tags
    
    * [192] Fixed model names and template tags

--- src/open_inwoner/components/templatetags/footer_tags.py
+++ src/open_inwoner/components/templatetags/footer_tags.py
@@ -4,10 +4,29 @@ register = template.Library()
 
 
 @register.inclusion_tag("components/Footer/Footer.html")
-def footer(logo_url, **kwargs):
-    return {**kwargs, "logo_url": logo_url}
+def footer(logo_url, footer_texts, **kwargs):
+    """
+    Describing information
+
+    Usage:
+        {% footer logo_url=settings.logo %}
+
+    Variables:
+        + logo_url: string | the location of the logo.
+        + footer_texts: dict | the dictionary with the footer texts.
+    """
+    return {**kwargs, "logo_url": logo_url, "footer_texts": footer_texts}
 
 
 @register.inclusion_tag("components/Footer/Social.html")
 def social(**kwargs):
+    """
+    Displaying the social links.
+
+    Usage:
+        {% social %}
+
+    Variables:
+        - primary: bool | If the primary styling should be used or not.
+    """
     return {**kwargs}
