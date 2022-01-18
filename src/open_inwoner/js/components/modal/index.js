1265c3235f00bf310a9ce5f49a1c710251f87f7a
Vasileios Zigras <zigras00@gmail.com>
2022-01-18 11:27:46 +0100

    [#182] Fixed linting

--- src/open_inwoner/js/components/modal/index.js
+++ src/open_inwoner/js/components/modal/index.js
@@ -1,26 +1,25 @@
-import swal from "sweetalert";
+import swal from 'sweetalert'
 
 class HelpModal {
-    constructor(helpButton) {
-        this.helpButton = helpButton
-        this.modal = document.querySelector(".help-modal")
-        this.helpButton.addEventListener('click', this.showModal.bind(this))
-    }
+  constructor(helpButton) {
+    this.helpButton = helpButton
+    this.modal = document.querySelector('.help-modal')
+    this.helpButton.addEventListener('click', this.showModal.bind(this))
+  }
 
-    showModal(event) {
-        event.preventDefault()
-        this.helpButton.classList.add('focus')
-        swal({
-            title: this.modal.dataset.helpTitle,
-            text: this.modal.dataset.helpText,
-            buttons: {
-                close: this.modal.dataset.helpClose
-            }
-        })
-        .then((close) => {
-            this.helpButton.classList.remove('focus')
-        })
-    }
+  showModal(event) {
+    event.preventDefault()
+    this.helpButton.classList.add('focus')
+    swal({
+      title: this.modal.dataset.helpTitle,
+      text: this.modal.dataset.helpText,
+      buttons: {
+        close: this.modal.dataset.helpClose,
+      },
+    }).then((close) => {
+      this.helpButton.classList.remove('focus')
+    })
+  }
 }
 
 const helpButton = document.querySelectorAll('.accessibility-modal')
