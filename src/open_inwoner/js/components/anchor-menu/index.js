1e2556b57ba5734da9f15f454410ef6b5e37195f
Jorik Kraaikamp <jorikkraaikamp@gmail.com>
2022-01-26 16:02:55 +0100

    Fixed that the anchor menu would not break other JavaScript files (#90)
    * Fixed that the anchor menu would work
    
    * Fixed the javascript linting

--- src/open_inwoner/js/components/anchor-menu/index.js
+++ src/open_inwoner/js/components/anchor-menu/index.js
@@ -1,6 +1,10 @@
 import Gumshoe from 'gumshoejs'
 
-new Gumshoe('.anchor-menu a', {
-  navClass: 'anchor-menu__list-item--active',
-  offset: 75,
-})
+const anchor = document.querySelectorAll('.anchor-menu')
+
+if (anchor.length > 0) {
+  new Gumshoe('.anchor-menu a', {
+    navClass: 'anchor-menu__list-item--active',
+    offset: 75,
+  })
+}
