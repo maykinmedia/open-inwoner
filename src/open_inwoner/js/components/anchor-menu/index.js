6fb3b7219b40425e49fb4f46b9fda5b096a84783
Jorik Kraaikamp <jorikkraaikamp@gmail.com>
2022-01-26 15:55:04 +0100

    Fixed the javascript linting

--- src/open_inwoner/js/components/anchor-menu/index.js
+++ src/open_inwoner/js/components/anchor-menu/index.js
@@ -1,10 +1,10 @@
 import Gumshoe from 'gumshoejs'
 
-const anchor = document.querySelectorAll('.anchor-menu');
+const anchor = document.querySelectorAll('.anchor-menu')
 
 if (anchor.length > 0) {
-    new Gumshoe('.anchor-menu a', {
+  new Gumshoe('.anchor-menu a', {
     navClass: 'anchor-menu__list-item--active',
     offset: 75,
-    })
+  })
 }
