21368a3affd169c3d27d8293bb9dba411a940f98
Sven van de Scheur <svenvandescheur@gmail.com>
2022-02-07 12:57:09 +0100

    :card_file_box: #278 - Add question model.

---
+++ src/open_inwoner/pdc/models/faq.py
@@ -0,0 +1,38 @@
+from django.db import models
+from ordered_model.models import OrderedModel, OrderedModelQuerySet, OrderedModelManager
+
+
+class QuestionQuerySet(OrderedModelQuerySet):
+    pass
+
+
+class QuestionManager(OrderedModelManager):
+    def get_queryset(self):
+        return QuestionQuerySet(self.model, using=self._db)
+
+
+class QuestionCategoryQuerySet(OrderedModelQuerySet):
+    pass
+
+
+class QuestionCategoryManager(OrderedModelManager):
+    def get_queryset(self):
+        return QuestionCategoryQuerySet(self.model, using=self._db)
+
+
+class Question(OrderedModel):
+    category = models.ForeignKey('pdc.Category', on_delete=models.CASCADE)
+    question = models.CharField('Vraag', max_length=250)
+    answer = models.TextField('Antwoord')
+
+    objects = QuestionManager()
+
+    order_with_respect_to = 'category'
+
+    class Meta(OrderedModel.Meta):
+        verbose_name = 'Vraag'
+        verbose_name_plural = 'FAQ vragen'
+        ordering = ('category', 'order')
+
+    def __str__(self):
+        return self.question
