9766368430bc4049664d4c00e54599aacadfe503
Jorik Kraaikamp <jorikkraaikamp@gmail.com>
2022-02-09 14:36:26 +0100

    better breadcrumbs

--- src/open_inwoner/components/templatetags/helpers.py
+++ src/open_inwoner/components/templatetags/helpers.py
@@ -1,4 +1,5 @@
 from django import template
+from django.urls import reverse
 
 register = template.Library()
 
@@ -18,3 +19,24 @@ def startswith(text, starts):
     if isinstance(text, str):
         return text.startswith(starts)
     return False
+
+
+@register.simple_tag(takes_context=True)
+def get_product_url(context, product):
+    """
+    Builds the product url build on the given product.
+
+    Usage:
+        {% get_product_url product=product as product_url %}
+        {% get_product_url product=product %}
+
+    Variables:
+        + product: Product | The product we should generate the url for.
+    """
+    category = context.get("parent")
+    if category:
+        return reverse(
+            "pdc:category_product_detail",
+            kwargs={"theme_slug": category.get_build_slug(), "slug": product.slug},
+        )
+    return product.get_absolute_url()
