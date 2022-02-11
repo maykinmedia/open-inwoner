8195497d65d80e47be548e271dd2306638821abd
Sven van de Scheur <svenvandescheur@gmail.com>
2022-02-11 09:50:40 +0100

    :ok_hand: - I'd suggest to add category to the list_display, it'll increase readability

--- src/open_inwoner/pdc/admin/faq.py
+++ src/open_inwoner/pdc/admin/faq.py
@@ -8,7 +8,7 @@ from ..models import Question
 @admin.register(Question)
 class QuestionAdmin(OrderedModelAdmin):
     list_filter = ("category",)
-    list_display = ("question", "move_up_down_links")
+    list_display = ("question", "category", "move_up_down_links")
     search_fields = (
         "question",
         "answer",
