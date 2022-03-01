5c420c359e218b00ced068010d337e1683d8bb43
Sven van de Scheur <svenvandescheur@gmail.com>
2022-03-01 11:33:58 +0100

    :ok_hand: - there is already a template tagrender_tags which does the same job, could you merge them into one? And some documentation would be nice

--- src/open_inwoner/components/templatetags/string_tags.py
+++ src/open_inwoner/components/templatetags/string_tags.py
@@ -11,30 +11,3 @@ register = template.Library()
 def addstr(arg1, arg2):
     """concatenate arg1 & arg2"""
     return str(arg1) + str(arg2)
-
-
-@register.filter()
-def markdown2html(value):
-    md = markdown.Markdown(extensions=["tables"])
-    html = md.convert(value)
-    soup = BeautifulSoup(html, "html.parser")
-    class_adders = [
-        ("h1", "h1"),
-        ("h2", "h2"),
-        ("h3", "h3"),
-        ("h4", "h4"),
-        ("h5", "h5"),
-        ("h6", "h6"),
-        ("img", "image"),
-        ("li", "li"),
-        ("p", "p"),
-        ("a", "link link--secondary"),
-        ("table", "table table--content"),
-        ("th", "table__header"),
-        ("td", "table__item"),
-    ]
-    for tag, class_name in class_adders:
-        for element in soup.find_all(tag):
-            element.attrs["class"] = class_name
-
-    return str(soup)
