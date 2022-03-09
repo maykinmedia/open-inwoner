bb5f644b431b77809d5bb4a0234a6177fdf22be2
vasileios <zigras00@gmail.com>
2022-03-09 13:43:27 +0100

    [#307] Added extra content for status detail page

--- src/open_inwoner/accounts/views/cases.py
+++ src/open_inwoner/accounts/views/cases.py
@@ -8,7 +8,11 @@ from django.views.generic import TemplateView
 from view_breadcrumbs import BaseBreadcrumbMixin
 
 from open_inwoner.openzaak.cases import fetch_cases, fetch_specific_case
-from open_inwoner.openzaak.statuses import fetch_status_history, fetch_status_types
+from open_inwoner.openzaak.statuses import (
+    fetch_case_information_objects,
+    fetch_status_history,
+    fetch_status_types,
+)
 
 
 class CasesListView(
@@ -38,6 +42,7 @@ class CasesListView(
         context["open_cases"].sort(key=lambda case: case.startdatum, reverse=True)
         context["closed_cases"] = [case for case in cases if case.einddatum]
         context["closed_cases"].sort(key=lambda case: case.einddatum, reverse=True)
+
         return context
 
 
@@ -71,18 +76,28 @@ class CasesStatusView(
         case_uuid = context["object_id"]
         case = fetch_specific_case(case_uuid)
 
-        if case:
-            statuses = fetch_status_history(case.url)
-            status_types = fetch_status_types(case.zaaktype)
+        if not case:
+            return context
 
-            status_types = {st.url: st for st in status_types}
-            for status in statuses:
-                status.statustype = status_types[status.statustype]
+        case_info_objects = fetch_case_information_objects(case.url)
+        statuses = fetch_status_history(case.url)
+        status_types = fetch_status_types(case.zaaktype)
 
-            context["case"] = case
-            context["statuses"] = statuses
-            context["statuses"].sort(
-                key=lambda status: status.datum_status_gezet, reverse=True
-            )
+        status_types = {st.url: st for st in status_types}
+        for status in statuses:
+            status.statustype = status_types.get(status.statustype)
+
+        # Sort list of statuses in order to able to get the most recent one
+        statuses.sort(key=lambda status: status.datum_status_gezet, reverse=True)
+
+        context["case"] = {
+            "obj": case,
+            "info": {
+                "count": len(case_info_objects),
+                "objects": case_info_objects,
+            },
+            "statuses": statuses,
+            "current_status": statuses[0] if statuses else None,
+        }
 
         return context
--- src/open_inwoner/openzaak/tests/test_statuses.py
+++ src/open_inwoner/openzaak/tests/test_statuses.py
@@ -15,11 +15,18 @@ from .factories import ServiceFactory
 
 ZAKEN_ROOT = "https://zaken.nl/api/v1/"
 CATALOGI_ROOT = "https://catalogi.nl/api/v1/"
+DOCUMENTEN_ROOT = "https://documenten.nl/api/v1/"
 
 
 @requests_mock.Mocker()
 class TestListStatusView(WebTest):
     def setUp(self):
+        self.user = UserFactory(
+            first_name="",
+            last_name="",
+            login_type=LoginTypeChoices.digid,
+            bsn="900222086",
+        )
         self.config = OpenZaakConfig.get_solo()
         self.zaak_service = ServiceFactory(api_root=ZAKEN_ROOT, api_type=APITypes.zrc)
         self.config.zaak_service = self.zaak_service
@@ -77,6 +84,17 @@ class TestListStatusView(WebTest):
             volgnummer=1,
             is_eindstatus=False,
         )
+        self.zaak_informatie_object = generate_oas_component(
+            "zrc",
+            "schemas/ZaakInformatieObject",
+            url=f"{ZAKEN_ROOT}zaakinformatieobjecten/e55153aa-ad2c-4a07-ae75-15add57d6",
+            informatieobject=f"{DOCUMENTEN_ROOT}enkelvoudiginformatieobjecten/014c38fe-b010-4412-881c-3000032fb812",
+            zaak=f"{ZAKEN_ROOT}zaken/d8bbdeb7-770f-4ca9-b1ea-77b4730bf67d",
+            aard_relatie_weergave="Hoort bij, omgekeerd: kent",
+            titel="",
+            beschrijving="",
+            registratiedatum="2021-01-12",
+        )
 
     def _setUpMocks(self, m):
         mock_service_oas_get(m, ZAKEN_ROOT, "zrc")
@@ -84,6 +102,10 @@ class TestListStatusView(WebTest):
         m.get(
             f"{ZAKEN_ROOT}zaken/d8bbdeb7-770f-4ca9-b1ea-77b4730bf67d",
             json=self.zaak,
+        )
+        m.get(
+            f"{ZAKEN_ROOT}zaakinformatieobjecten?zaak={self.zaak['url']}",
+            json=[self.zaak_informatie_object],
         )
         m.get(
             f"{ZAKEN_ROOT}statussen",
@@ -96,22 +118,21 @@ class TestListStatusView(WebTest):
 
     def test_status_is_retrieved_when_user_logged_in_via_digid(self, m):
         self._setUpMocks(m)
-        user = UserFactory(
-            first_name="",
-            last_name="",
-            login_type=LoginTypeChoices.digid,
-            bsn="900222086",
-        )
+
         response = self.app.get(
             reverse(
                 "accounts:case_status",
                 kwargs={"object_id": "d8bbdeb7-770f-4ca9-b1ea-77b4730bf67d"},
             ),
-            user=user,
+            user=self.user,
         )
-        statuses = [status.url for status in response.context["statuses"]]
+
+        statuses = [
+            status.url for status in response.context.get("case", {}).get("statuses")
+        ]
         status_types = [
-            status.statustype.url for status in response.context["statuses"]
+            status.statustype.url
+            for status in response.context.get("case", {}).get("statuses")
         ]
 
         self.assertListEqual(
@@ -128,6 +149,44 @@ class TestListStatusView(WebTest):
                 f"{CATALOGI_ROOT}statustypen/e3798107-ab27-4c3c-977d-7afb71e71",
                 f"{CATALOGI_ROOT}statustypen/e3798107-ab27-4c3c-977d-7afb71e71fe4",
             ],
+        )
+
+    def test_current_status_in_context_is_the_most_recent_one(self, m):
+        self._setUpMocks(m)
+
+        response = self.app.get(
+            reverse(
+                "accounts:case_status",
+                kwargs={"object_id": "d8bbdeb7-770f-4ca9-b1ea-77b4730bf67d"},
+            ),
+            user=self.user,
+        )
+        current_status = response.context.get("case", {}).get("current_status")
+
+        self.assertEquals(
+            current_status.url,
+            f"{ZAKEN_ROOT}statussen/3da89990-c7fc-476a-ad13-c8b22b60083c",
+        )
+
+    def test_case_information_objects_are_retrieved_when_user_logged_in_via_digid(
+        self, m
+    ):
+        self._setUpMocks(m)
+
+        response = self.app.get(
+            reverse(
+                "accounts:case_status",
+                kwargs={"object_id": "d8bbdeb7-770f-4ca9-b1ea-77b4730bf67d"},
+            ),
+            user=self.user,
+        )
+
+        self.assertEquals(
+            response.context.get("case", {}).get("info", {}).get("count"), 1
+        )
+        self.assertEquals(
+            response.context.get("case", {}).get("info", {}).get("objects")[0].url,
+            f"{ZAKEN_ROOT}zaakinformatieobjecten/e55153aa-ad2c-4a07-ae75-15add57d6",
         )
 
     def test_user_is_redirected_to_root_when_not_logged_in_via_digid(self, m):
@@ -164,6 +223,24 @@ class TestListStatusView(WebTest):
             f"{reverse('login')}?next={reverse('accounts:case_status', kwargs={'object_id': 'd8bbdeb7-770f-4ca9-b1ea-77b4730bf67d'})}",
         )
 
+    def test_missing_catalogi_client_returns_statustype_none(self, m):
+        self._setUpMocks(m)
+
+        self.config.catalogi_service = None
+        self.config.save()
+
+        response = self.app.get(
+            reverse(
+                "accounts:case_status",
+                kwargs={"object_id": "d8bbdeb7-770f-4ca9-b1ea-77b4730bf67d"},
+            ),
+            user=self.user,
+        )
+
+        self.assertIsNone(
+            response.context.get("case", {}).get("current_status").statustype
+        )
+
     def test_no_status_is_retrieved_when_http_404(self, m):
         mock_service_oas_get(m, ZAKEN_ROOT, "zrc")
         mock_service_oas_get(m, CATALOGI_ROOT, "ztc")
@@ -172,29 +249,27 @@ class TestListStatusView(WebTest):
             json=self.zaak,
         )
         m.get(
+            f"{ZAKEN_ROOT}zaakinformatieobjecten?zaak={self.zaak['url']}",
+            status_code=404,
+        )
+        m.get(
             f"{ZAKEN_ROOT}statussen",
             status_code=404,
         )
         m.get(
             f"{CATALOGI_ROOT}statustypen",
-            json=paginated_response([self.status_type1, self.status_type2]),
+            status_code=404,
         )
 
-        user = UserFactory(
-            first_name="",
-            last_name="",
-            login_type=LoginTypeChoices.digid,
-            bsn="900222086",
-        )
         response = self.app.get(
             reverse(
                 "accounts:case_status",
                 kwargs={"object_id": "d8bbdeb7-770f-4ca9-b1ea-77b4730bf67d"},
             ),
-            user=user,
+            user=self.user,
         )
 
-        self.assertListEqual(response.context["statuses"], [])
+        self.assertListEqual(response.context.get("case", {}).get("statuses"), [])
 
     def test_no_status_is_retrieved_when_http_500(self, m):
         mock_service_oas_get(m, ZAKEN_ROOT, "zrc")
@@ -204,26 +279,24 @@ class TestListStatusView(WebTest):
             json=self.zaak,
         )
         m.get(
+            f"{ZAKEN_ROOT}zaakinformatieobjecten?zaak={self.zaak['url']}",
+            status_code=500,
+        )
+        m.get(
             f"{ZAKEN_ROOT}statussen",
             status_code=500,
         )
         m.get(
             f"{CATALOGI_ROOT}statustypen",
-            json=paginated_response([self.status_type1, self.status_type2]),
+            status_code=500,
         )
 
-        user = UserFactory(
-            first_name="",
-            last_name="",
-            login_type=LoginTypeChoices.digid,
-            bsn="900222086",
-        )
         response = self.app.get(
             reverse(
                 "accounts:case_status",
                 kwargs={"object_id": "d8bbdeb7-770f-4ca9-b1ea-77b4730bf67d"},
             ),
-            user=user,
+            user=self.user,
         )
 
-        self.assertListEqual(response.context["statuses"], [])
+        self.assertListEqual(response.context.get("case", {}).get("statuses"), [])
