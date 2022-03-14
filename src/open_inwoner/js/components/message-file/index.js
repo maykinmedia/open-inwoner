c4597e5048c6f1f3d86d1075635bbb9f88bd0c58
Anna Shamray <anna.shamray@maykinmedia.nl>
2022-03-14 12:16:00 +0100

    :lipstick: style file uploading for messages

--- src/open_inwoner/js/components/message-file/index.js
+++ src/open_inwoner/js/components/message-file/index.js
@@ -1,27 +1,41 @@
 class MessageFile {
-    constructor(node) {
-        this.node = node;
-        this.fileInput = node.querySelector('input[type=file]');
-        this.fileInput.addEventListener('change', this.changeFile.bind(this))
-        this.preview = node.querySelector('.preview')
+  constructor(node) {
+    this.node = node
+    this.fileInput = node.querySelector('input[type=file]')
+    this.fileInput.addEventListener('change', this.changeFile.bind(this))
+  }
+
+  changeFile(event) {
+    const files = event.target.files
+    if (files.length) {
+      const file = files[0]
+      this.addPreview(file)
     }
+  }
 
-    changeFile(event) {
-        const files = event.target.files;
-        if (files.length) {
-            const file = files[0];
-            // const para = document.createElement('p');
-            // para.textContent
+  removeFile(event) {
+    console.log(event.target)
+    event.target.parentNode.remove()
+  }
 
-            this.preview.textContent = file.name;
+  addPreview(file) {
+    const preview = document.createElement('div')
+    preview.classList.add('message-file__preview')
+    this.node.appendChild(preview)
 
-        }
+    const fileName = document.createElement('span')
+    fileName.textContent = file.name
+    preview.appendChild(fileName)
 
-        console.log(files);
-    }
+    const deleteIcon = document.createElement('span')
+    deleteIcon.classList.add('message-file__delete')
+    deleteIcon.classList.add('material-icons')
+    deleteIcon.textContent = 'delete'
+    preview.appendChild(deleteIcon)
 
+    deleteIcon.addEventListener('click', this.removeFile.bind(this))
+  }
 }
-
 
 const messageFiles = document.querySelectorAll('.message-file')
 ;[...messageFiles].forEach((node) => new MessageFile(node))
