2534a40c527bb1138ceea09605198f2067308b97
Anna Shamray <anna.shamray@maykinmedia.nl>
2022-03-31 16:41:03 +0200

    :recycle: refactor search autocomplete with autocompleteJS

--- src/open_inwoner/js/components/autocomplete-search/index.js
+++ src/open_inwoner/js/components/autocomplete-search/index.js
@@ -1,44 +1,37 @@
+import autoComplete from '@tarekraafat/autocomplete.js/dist/autoComplete'
+
 const query = document.querySelector('#search-form #id_query')
 const autocompleteUrl = '/api/search/autocomplete/'
 
 const addAutocomplete = (node) => {
-  // turn off build in autocomplete
   node.setAttribute('autocomplete', 'off')
 
-  node.addEventListener('input', function () {
-    removeAutocomplete()
-
-    const val = this.value
-    const url = `${autocompleteUrl}?search=${val}`
-
-    fetch(url)
-      .then((response) => response.json())
-      .then((data) => {
-        const options = data.options
-
-        const ul = document.createElement('div')
-        ul.setAttribute('class', 'autocomplete')
-        node.parentNode.parentNode.appendChild(ul)
-
-        options.forEach((option) => {
-          const li = document.createElement('div')
-          li.setAttribute('class', 'autocomplete__item')
-          li.innerHTML = option
-          li.addEventListener('click', () => {
-            node.value = option
-            removeAutocomplete()
-          })
-          ul.appendChild(li)
-        })
-      })
-      .catch((error) => {
-        console.error('Error:', error)
-      })
+  const autoCompleteJS = new autoComplete({
+    selector: '#search-form #id_query',
+    data: {
+      src: async (query) => {
+        try {
+          const source = await fetch(`${autocompleteUrl}?search=${query}`)
+          const data = await source.json()
+          return data.options
+        } catch (error) {
+          console.error('Error:', error)
+        }
+      },
+    },
+    debounce: 300, // Milliseconds
+    resultItem: {
+      highlight: true,
+    },
+    events: {
+      input: {
+        selection: (event) => {
+          node.value = event.detail.selection.value
+        },
+      },
+    },
   })
-}
-
-const removeAutocomplete = () => {
-  document.querySelectorAll('.autocomplete').forEach((el) => el.remove())
+  return autoCompleteJS
 }
 
 if (query) {
