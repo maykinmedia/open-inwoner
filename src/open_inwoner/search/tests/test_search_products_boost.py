e8e19239661c063299060919ca6986e4386d3697
Anna Shamray <anna.shamray@maykinmedia.nl>
2022-03-31 14:57:38 +0200

    :white_check_mark: test field boosting in search

---
+++ src/open_inwoner/search/tests/test_search_products_boost.py
@@ -0,0 +1,51 @@
+from django.test import TestCase
+
+from open_inwoner.pdc.tests.factories import ProductFactory
+
+from ..searches import search_products
+from .utils import ESMixin
+from ..models import FieldBoost
+
+
+class SearchBoostTests(ESMixin, TestCase):
+    def setUp(self):
+        super().setUp()
+
+        # clear boosts
+        FieldBoost.objects.all().delete()
+
+        # add products
+        self.product_name = ProductFactory.create(
+            name="Some found",
+            summary="Some",
+            content="Some",
+            keywords=["Some"],
+        )
+        self.product_summary = ProductFactory.create(
+            name="Other", summary="Other found", content="Other", keywords=["other"]
+        )
+        self.product_content = ProductFactory.create(
+            name="Any", summary="Any", content="Any found", keywords=["Any"]
+        )
+        self.update_index()
+
+    def test_boost_name(self):
+        FieldBoost.objects.create(field="name", boost=10)
+        results = search_products("found").results
+
+        self.assertEqual(len(results), 3)
+        self.assertEqual(int(results[0].meta.id), self.product_name.id)
+
+    def test_boost_summary(self):
+        FieldBoost.objects.create(field="summary", boost=10)
+        results = search_products("found").results
+
+        self.assertEqual(len(results), 3)
+        self.assertEqual(int(results[0].meta.id), self.product_summary.id)
+
+    def test_boost_content(self):
+        FieldBoost.objects.create(field="content", boost=10)
+        results = search_products("found").results
+
+        self.assertEqual(len(results), 3)
+        self.assertEqual(int(results[0].meta.id), self.product_content.id)
