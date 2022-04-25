3f4e3e5b311b952427f33a9161143898d8ba7fe0
Vasileios Zigras <zigras00@gmail.com>
2022-04-25 09:18:36 +0200

    [#403-#405] Feature/log admin actions (#193)
    * [#403] Added logs for add,change,delete in admin
    
    * [#403] Improved logging and added tests

---
+++ src/open_inwoner/utils/signals.py
@@ -0,0 +1,30 @@
+import logging
+
+from django.contrib.admin import models
+from django.db.models.signals import pre_save
+from django.dispatch import receiver
+
+from timeline_logger.models import TimelineLog
+
+logger = logging.getLogger(__name__)
+
+
+@receiver(pre_save, sender=models.LogEntry)
+def copy_log_entry_to_timeline_logger(sender, **kwargs):
+    log_entry = kwargs.get("instance")
+    TimelineLog.objects.create(
+        timestamp=log_entry.action_time,
+        object_id=log_entry.object_id,
+        content_type=log_entry.content_type,
+        user=log_entry.user,
+        extra_data={
+            "content_object_repr": log_entry.object_repr or "",
+            "action_flag": log_entry.action_flag,
+            "message": log_entry.get_change_message(),
+        },
+    )
+    logger.info(
+        "Modified: %s, %s",
+        log_entry.content_type,
+        log_entry.get_change_message(),
+    )
