daac74f878de6adca6e31c2b50b7b8b94235cac5
Alex de Landgraaf <alex@maykinmedia.nl>
2022-05-09 17:56:39 +0200

    Revert ":sparkles: #481 - Handle situation where the user has been made inact…"

--- src/open_inwoner/components/templatetags/messages_tags.py
+++ src/open_inwoner/components/templatetags/messages_tags.py
@@ -34,7 +34,6 @@ def messages(
         + status: string | The status below the subject.
 
     Extra context:
-        - disabled: bool | Whether the form is disabled.
         - days: set | the message_list grouped by date.
     """
 
@@ -78,7 +77,6 @@ def messages(
 
     return {
         "days": get_messages_by_date(message_list),
-        "disabled": form.fields["content"].disabled,
         "form": form,
         "me": me,
         "status": status,
