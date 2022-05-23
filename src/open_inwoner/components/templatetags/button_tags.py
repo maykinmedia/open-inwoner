a60f691af3d40a73bdd88f9454a9f9f9d0eace65
Jorik Kraaikamp <jorikkraaikamp@gmail.com>
2022-05-23 10:40:16 +0200

    Added the button on mobile (#233)

--- src/open_inwoner/components/templatetags/button_tags.py
+++ src/open_inwoner/components/templatetags/button_tags.py
@@ -20,6 +20,7 @@ def button_row(parser, token):
 
     Variables:
         - align: enum[right] | if the buttons should be aligned left (no align should be given) or alinged right side.
+        - mobile: booleam | If the button row should only be displayed on mobile screens.
 
     Extra context:
         - contents: string (HTML) | this is the context between the button_row and endbutton_row tags
