daf2d46e649a671946454c13f2e7b044b7221934
Jorik Kraaikamp <jorikkraaikamp@gmail.com>
2022-05-23 15:57:52 +0200

    Fixed the duplicate and default media subfolder

--- src/open_inwoner/conf/base.py
+++ src/open_inwoner/conf/base.py
@@ -230,31 +230,10 @@ STATICFILES_FINDERS = [
 ]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
-MEDIA_SUBFOLDER = config("MEDIA_SUBFOLDER", "appels")
+MEDIA_SUBFOLDER = config("MEDIA_SUBFOLDER", "")
 
 if MEDIA_SUBFOLDER:
     MEDIA_ROOT = os.path.join(MEDIA_ROOT, MEDIA_SUBFOLDER)
-
-FILER_STORAGES = {
-    "public": {
-        "main": {
-            "ENGINE": "filer.storage.PublicFileSystemStorage",
-            "OPTIONS": {
-                "location": "/path/to/media/filer",
-                "base_url": "/media/filer/",
-            },
-            "UPLOAD_TO": "filer.utils.generate_filename.randomized",
-            "UPLOAD_TO_PREFIX": "filer_public",
-        },
-        "thumbnails": {
-            "ENGINE": "filer.storage.PublicFileSystemStorage",
-            "OPTIONS": {
-                "location": "/path/to/media/filer_thumbnails",
-                "base_url": "/media/filer_thumbnails/",
-            },
-        },
-    },
-}
 
 MEDIA_URL = "/media/"
 
