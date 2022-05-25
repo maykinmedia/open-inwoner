6cbdd75d972401bce38b544ee66fd88ef007db60
Jorik Kraaikamp <jorikkraaikamp@gmail.com>
2022-05-25 12:27:44 +0200

    Fixed the digid logo

--- src/open_inwoner/conf/staging.py
+++ src/open_inwoner/conf/staging.py
@@ -8,4 +8,6 @@ os.environ.setdefault("ENVIRONMENT", "staging")
 # NOTE: watch out for multiple projects using the same cache!
 os.environ.setdefault("CACHE_DEFAULT", "127.0.0.1:6379/1")
 
+CSP_REPORTS_SAVE = True
+
 from .production import *  # noqa isort:skip
