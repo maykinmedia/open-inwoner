7b825c1c17240db592a81d86e18ddb8aa6569dd3
Anna Shamray <anna.shamray@maykinmedia.nl>
2022-05-27 17:50:35 +0200

    :green_heart: mark that hash is not used for security

--- src/open_inwoner/utils/hash.py
+++ src/open_inwoner/utils/hash.py
@@ -3,6 +3,6 @@ from hashlib import md5
 
 def generate_email_from_string(value: str) -> str:
     """generate email address based on string"""
-    hashed_bsn = md5(value.encode()).hexdigest()
+    hashed_bsn = md5(value.encode(), usedforsecurity=False).hexdigest()
 
     return f"{hashed_bsn}@example.org"
