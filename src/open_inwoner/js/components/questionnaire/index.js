f140f0d849123358ec442448e29ab9a6fd325a15
vasileios <zigras00@gmail.com>
2022-05-27 09:43:35 +0200

    [#618] Fixed scrolling according to PR review

--- src/open_inwoner/js/components/questionnaire/index.js
+++ src/open_inwoner/js/components/questionnaire/index.js
@@ -1,10 +1,13 @@
 const question = document.getElementById('question')
-const stepIndicators = document.querySelectorAll(
+const stepIndicator = document.querySelector(
   '.step-indicator__list-item--active'
 )
 
-stepIndicators.forEach((stepIndicator) => {
+if (stepIndicator && question) {
+  /*
+   * Focus on the question only if it's the not the first one and if it's accessed via a mobile phone
+   */
   if (window.innerWidth < 768 && stepIndicator.dataset.step > 1) {
     question.scrollIntoView()
   }
-})
+}
