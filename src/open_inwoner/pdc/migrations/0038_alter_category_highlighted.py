46d74e6ba0c85e336a9ab1084cd768c855b6065b
Jorik Kraaikamp <jorikkraaikamp@gmail.com>
2022-06-01 10:43:31 +0200

    Update 0038_alter_category_highlighted.py

--- src/open_inwoner/pdc/migrations/0038_alter_category_highlighted.py
+++ src/open_inwoner/pdc/migrations/0038_alter_category_highlighted.py
@@ -6,13 +6,17 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('pdc', '0037_category_highlighted'),
+        ("pdc", "0037_category_highlighted"),
     ]
 
     operations = [
         migrations.AlterField(
-            model_name='category',
-            name='highlighted',
-            field=models.BooleanField(default=False, help_text='Wether the category should be highlighted or not.', verbose_name='Highlighted'),
+            model_name="category",
+            name="highlighted",
+            field=models.BooleanField(
+                default=False,
+                help_text="Wether the category should be highlighted or not.",
+                verbose_name="Highlighted",
+            ),
         ),
     ]
