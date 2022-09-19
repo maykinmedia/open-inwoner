b742df2373d86021239c41d766d79059cacf4a25
Alex de Landgraaf <alex@maykinmedia.nl>
2022-09-19 14:29:49 +0200

    Ensuring password-reset test is more deterministic by clearing cache before performing throttled requests

--- src/open_inwoner/accounts/tests/test_password_reset_view.py
+++ src/open_inwoner/accounts/tests/test_password_reset_view.py
@@ -1,3 +1,4 @@
+from django.core.cache import cache
 from django.test import TestCase
 from django.urls import reverse
 
@@ -5,6 +6,7 @@ from django.urls import reverse
 class PasswordResetViewTests(TestCase):
     def test_user_cant_access_the_password_reset_view_more_than_5_times(self):
         url = reverse("admin_password_reset")
+        cache.clear()
 
         for i in range(5):
             response = self.client.get(url)
