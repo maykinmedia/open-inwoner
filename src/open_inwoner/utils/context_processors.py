c17eb679b6999d7820a08515c20c5e758ac9f0c9
Bart van der Schoor <bart@maykinmedia.nl>
2022-11-10 15:39:58 +0100

    [#879] Updated context key from 'widgets' to 'questionnaire_page'

--- src/open_inwoner/utils/context_processors.py
+++ src/open_inwoner/utils/context_processors.py
@@ -41,7 +41,7 @@ def settings(request):
                 "theme_title": config.theme_title,
                 "theme_intro": config.theme_intro,
             },
-            "widgets": {
+            "questionnaire_page": {
                 "select_questionnaire_title": config.select_questionnaire_title,
                 "select_questionnaire_subtitle": config.select_questionnaire_subtitle,
             },
