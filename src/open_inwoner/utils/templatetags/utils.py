accfb8ac64ea8b7609d0f1890b58a43f6894d27f
Sven van de Scheur <svenvandescheur@gmail.com>
2022-11-22 12:17:02 +0100

    :green_heart: - Attempt to fix the tests.

--- src/open_inwoner/utils/templatetags/utils.py
+++ src/open_inwoner/utils/templatetags/utils.py
@@ -44,7 +44,8 @@ def multiply(value, times):
     """
     Multiplies value by times.
     """
-    return value*times
+    return value * times
+
 
 @register.simple_tag
 def placekitten(width=800, height=600):
