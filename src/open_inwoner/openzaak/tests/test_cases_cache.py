affd0f9f8c3366c27c8f1d105d19bf2014554291
Anna Shamray <anna.shamray@maykinmedia.nl>
2022-11-25 16:08:53 +0100

    :white_check_mark: [#892] update cache tests for case list views

---
+++ src/open_inwoner/openzaak/tests/test_cases_cache.py
@@ -0,0 +1,429 @@
+import datetime
+
+from django.core.cache import cache
+from django.urls import reverse_lazy
+
+import requests_mock
+from django_webtest import WebTest
+from freezegun import freeze_time
+from furl import furl
+from zgw_consumers.api_models.constants import VertrouwelijkheidsAanduidingen
+from zgw_consumers.constants import APITypes
+from zgw_consumers.test import generate_oas_component, mock_service_oas_get
+
+from open_inwoner.accounts.choices import LoginTypeChoices
+from open_inwoner.accounts.tests.factories import UserFactory
+from open_inwoner.utils.test import ClearCachesMixin, paginated_response
+
+from ..models import OpenZaakConfig
+from .factories import ServiceFactory
+
+ZAKEN_ROOT = "https://zaken.nl/api/v1/"
+CATALOGI_ROOT = "https://catalogi.nl/api/v1/"
+
+
+@requests_mock.Mocker()
+class OpenCaseListCacheTests(ClearCachesMixin, WebTest):
+    url = reverse_lazy("accounts:my_open_cases")
+
+    @classmethod
+    def setUpTestData(cls):
+        super().setUpTestData()
+
+        cls.user = UserFactory(
+            login_type=LoginTypeChoices.digid, bsn="900222086", email="johm@smith.nl"
+        )
+        # services
+        cls.zaak_service = ServiceFactory(api_root=ZAKEN_ROOT, api_type=APITypes.zrc)
+        cls.catalogi_service = ServiceFactory(
+            api_root=CATALOGI_ROOT, api_type=APITypes.ztc
+        )
+        # openzaak config
+        cls.config = OpenZaakConfig.get_solo()
+        cls.config.zaak_service = cls.zaak_service
+        cls.config.catalogi_service = cls.catalogi_service
+        cls.config.zaak_max_confidentiality = (
+            VertrouwelijkheidsAanduidingen.beperkt_openbaar
+        )
+        cls.config.save()
+
+        cls.zaak1 = generate_oas_component(
+            "zrc",
+            "schemas/Zaak",
+            url=f"{ZAKEN_ROOT}zaken/d8bbdeb7-770f-4ca9-b1ea-77b4730bf67d",
+            zaaktype=f"{CATALOGI_ROOT}zaaktypen/53340e34-7581-4b04-884f",
+            identificatie="ZAAK-2022-0000000001",
+            omschrijving="Coffee zaak1",
+            startdatum="2022-01-02",
+            einddatum=None,
+            status=f"{ZAKEN_ROOT}statussen/3da89990-c7fc-476a-ad13-c9023450083c",
+            vertrouwelijkheidaanduiding=VertrouwelijkheidsAanduidingen.openbaar,
+        )
+        cls.zaak2 = generate_oas_component(
+            "zrc",
+            "schemas/Zaak",
+            url=f"{ZAKEN_ROOT}zaken/e4d469b9-6666-4bdd-bf42-b53445298102",
+            zaaktype=f"{CATALOGI_ROOT}zaaktypen/53340e34-7581-4b04-884f",
+            identificatie="ZAAK-2022-0008800002",
+            omschrijving="Coffee zaak2",
+            startdatum="2022-01-12",
+            einddatum=None,
+            status=f"{ZAKEN_ROOT}statussen/3da81560-c7fc-476a-ad13-beu760sle929",
+            vertrouwelijkheidaanduiding=VertrouwelijkheidsAanduidingen.openbaar,
+        )
+        cls.zaaktype = generate_oas_component(
+            "ztc",
+            "schemas/ZaakType",
+            url=f"{CATALOGI_ROOT}zaaktypen/53340e34-7581-4b04-884f",
+            omschrijving="Coffee zaaktype",
+            catalogus=f"{CATALOGI_ROOT}catalogussen/1b643db-81bb-d71bd5a2317a",
+            vertrouwelijkheidaanduiding="openbaar",
+        )
+        cls.status1 = generate_oas_component(
+            "zrc",
+            "schemas/Zaak",
+            url=f"{ZAKEN_ROOT}statussen/3da81560-c7fc-476a-ad13-beu760sle929",
+            zaak=f"{ZAKEN_ROOT}zaken/e4d469b9-6666-4bdd-bf42-b53445298102",
+            statustype=f"{CATALOGI_ROOT}statustypen/e3798107-ab27-4c3c-977d-777yu878km09",
+            datum_status_gezet="2021-01-12",
+            statustoelichting="",
+        )
+        cls.status2 = generate_oas_component(
+            "zrc",
+            "schemas/Zaak",
+            url=f"{ZAKEN_ROOT}statussen/3da89990-c7fc-476a-ad13-c9023450083c",
+            zaak=f"{ZAKEN_ROOT}zaken/d8bbdeb7-770f-4ca9-b1ea-77b4730bf67d",
+            statustype=f"{CATALOGI_ROOT}statustypen/e3798107-ab27-4c3c-977d-744516671fe4",
+            datum_status_gezet="2021-03-12",
+            statustoelichting="",
+        )
+        cls.status_type1 = generate_oas_component(
+            "ztc",
+            "schemas/StatusType",
+            url=f"{CATALOGI_ROOT}statustypen/e3798107-ab27-4c3c-977d-777yu878km09",
+            zaaktype=f"{CATALOGI_ROOT}zaaktypen/53340e34-7581-4b04-884f-8ff7e6a73c2c",
+            omschrijving="Initial request",
+            volgnummer=1,
+            is_eindstatus=False,
+        )
+        cls.status_type2 = generate_oas_component(
+            "ztc",
+            "schemas/StatusType",
+            url=f"{CATALOGI_ROOT}statustypen/e3798107-ab27-4c3c-977d-744516671fe4",
+            zaaktype=f"{CATALOGI_ROOT}zaaktypen/53340e34-7581-4b04-884f-8ff7e6a73c2c",
+            omschrijving="Finish",
+            volgnummer=2,
+            is_eindstatus=True,
+        )
+
+        # objects needed to test how the cache is updated
+        cls.new_zaak = generate_oas_component(
+            "zrc",
+            "schemas/Zaak",
+            url=f"{ZAKEN_ROOT}zaken/a25b2dce-1cae-4fc9-b9e9-141b0ad5189f",
+            zaaktype=f"{CATALOGI_ROOT}zaaktypen/53340e34-7581-4b04-884f-98ui7y87i876",
+            identificatie="ZAAK-2022-0000000003",
+            omschrijving="Tea zaak",
+            startdatum="2022-01-02",
+            einddatum=None,
+            status=f"{ZAKEN_ROOT}statussen/3da81560-c7fc-476a-ad13-oie8u899923g",
+        )
+        cls.new_zaaktype = generate_oas_component(
+            "ztc",
+            "schemas/ZaakType",
+            url=f"{CATALOGI_ROOT}zaaktypen/53340e34-7581-4b04-884f-98ui7y87i876",
+            omschrijving="Tea zaaktype",
+            catalogus=f"{CATALOGI_ROOT}catalogussen/1b643db-81bb-d71bd5a2317a",
+            vertrouwelijkheidaanduiding="openbaar",
+        )
+        cls.new_status = generate_oas_component(
+            "zrc",
+            "schemas/Zaak",
+            url=f"{ZAKEN_ROOT}statussen/3da81560-c7fc-476a-ad13-oie8u899923g",
+            zaak=f"{ZAKEN_ROOT}zaken/a25b2dce-1cae-4fc9-b9e9-141b0ad5189f",
+            statustype=f"{CATALOGI_ROOT}statustypen/e3798107-ab27-4c3c-977d-984yr8887rhe",
+            datum_status_gezet="2021-01-12",
+            statustoelichting="",
+        )
+        cls.new_status_type = generate_oas_component(
+            "ztc",
+            "schemas/StatusType",
+            url=f"{CATALOGI_ROOT}statustypen/e3798107-ab27-4c3c-977d-984yr8887rhe",
+            zaaktype=f"{CATALOGI_ROOT}zaaktypen/53340e34-7581-4b04-884f-98ui7y87i876",
+            omschrijving="Process",
+            volgnummer=1,
+            is_eindstatus=False,
+        )
+
+    def _setUpMocks(self, m):
+        mock_service_oas_get(m, ZAKEN_ROOT, "zrc")
+        mock_service_oas_get(m, CATALOGI_ROOT, "ztc")
+        m.get(
+            furl(f"{ZAKEN_ROOT}zaken")
+            .add(
+                {
+                    "rol__betrokkeneIdentificatie__natuurlijkPersoon__inpBsn": self.user.bsn,
+                    "maximaleVertrouwelijkheidaanduiding": VertrouwelijkheidsAanduidingen.beperkt_openbaar,
+                }
+            )
+            .url,
+            json=paginated_response([self.zaak1, self.zaak2]),
+        )
+        m.get(f"{CATALOGI_ROOT}zaaktypen/53340e34-7581-4b04-884f", json=self.zaaktype)
+        m.get(
+            f"{CATALOGI_ROOT}statustypen",
+            json=paginated_response([self.status_type1, self.status_type2]),
+        )
+        m.get(
+            f"{ZAKEN_ROOT}statussen/3da81560-c7fc-476a-ad13-beu760sle929",
+            json=self.status1,
+        )
+        m.get(
+            f"{ZAKEN_ROOT}statussen/3da89990-c7fc-476a-ad13-c9023450083c",
+            json=self.status2,
+        )
+        m.get(
+            f"{CATALOGI_ROOT}statustypen/e3798107-ab27-4c3c-977d-777yu878km09",
+            json=self.status_type1,
+        )
+        m.get(
+            f"{CATALOGI_ROOT}statustypen/e3798107-ab27-4c3c-977d-744516671fe4",
+            json=self.status_type2,
+        )
+
+    def test_case_types_are_cached(self, m):
+        self._setUpMocks(m)
+
+        # Cache is empty before the request
+        self.assertIsNone(cache.get(f"case_type:{self.zaaktype['url']}"))
+
+        self.app.get(self.url, user=self.user)
+
+        # Case type is cached after the request
+        self.assertIsNotNone(cache.get(f"case_type:{self.zaaktype['url']}"))
+
+    def test_cached_case_types_are_deleted_after_one_day(self, m):
+        self._setUpMocks(m)
+
+        with freeze_time("2022-01-01 12:00") as frozen_time:
+            self.app.get(self.url, user=self.user)
+
+            # After one day the results should be deleted
+            frozen_time.tick(delta=datetime.timedelta(days=1))
+            self.assertIsNone(cache.get(f"case_type:{self.zaaktype['url']}"))
+
+    def test_cached_case_types_in_combination_with_new_ones(self, m):
+        self._setUpMocks(m)
+
+        # First attempt
+        self.assertIsNone(cache.get(f"case_type:{self.zaaktype['url']}"))
+
+        self.app.get(self.url, user=self.user)
+
+        self.assertIsNotNone(cache.get(f"case_type:{self.zaaktype['url']}"))
+
+        # Second attempt with new case and case type
+        m.get(
+            f"{ZAKEN_ROOT}zaken?rol__betrokkeneIdentificatie__natuurlijkPersoon__inpBsn=900222086",
+            json=paginated_response([self.zaak1, self.zaak2, self.new_zaak]),
+        )
+        m.get(
+            f"{CATALOGI_ROOT}zaaktypen/53340e34-7581-4b04-884f-98ui7y87i876",
+            json=self.new_zaaktype,
+        )
+        m.get(
+            f"{CATALOGI_ROOT}statustypen",
+            json=paginated_response(
+                [self.status_type1, self.status_type2, self.new_status_type]
+            ),
+        )
+        m.get(
+            f"{CATALOGI_ROOT}statustypen/e3798107-ab27-4c3c-977d-984yr8887rhe",
+            json=self.new_status_type,
+        )
+        m.get(
+            f"{ZAKEN_ROOT}statussen/3da81560-c7fc-476a-ad13-oie8u899923g",
+            json=self.new_status,
+        )
+
+        self.assertIsNone(cache.get(f"case_type:{self.new_zaaktype['url']}"))
+
+        self.app.get(self.url, user=self.user)
+
+        self.assertIsNotNone(cache.get(f"case_type:{self.zaaktype['url']}"))
+        self.assertIsNotNone(cache.get(f"case_type:{self.new_zaaktype['url']}"))
+
+    def test_status_types_are_cached(self, m):
+        self._setUpMocks(m)
+
+        # Cache is empty before the request
+        self.assertIsNone(
+            cache.get(f"status_types_for_case_type:{self.zaaktype['url']}")
+        )
+        self.assertIsNone(
+            cache.get(f"status_types_for_case_type:{self.zaaktype['url']}")
+        )
+
+        self.app.get(self.url, user=self.user)
+
+        # Case type is cached after the request
+        self.assertIsNotNone(
+            cache.get(f"status_types_for_case_type:{self.zaaktype['url']}")
+        )
+        self.assertIsNotNone(
+            cache.get(f"status_types_for_case_type:{self.zaaktype['url']}")
+        )
+
+    def test_cached_status_types_are_deleted_after_one_day(self, m):
+        self._setUpMocks(m)
+
+        with freeze_time("2022-01-01 12:00") as frozen_time:
+            self.app.get(self.url, user=self.user)
+
+            # After one day the results should be deleted
+            frozen_time.tick(delta=datetime.timedelta(hours=24))
+            self.assertIsNone(
+                cache.get(f"status_types_for_case_type:{self.zaaktype['url']}")
+            )
+            self.assertIsNone(
+                cache.get(f"status_types_for_case_type:{self.zaaktype['url']}")
+            )
+
+    def test_cached_status_types_in_combination_with_new_ones(self, m):
+        self._setUpMocks(m)
+
+        # First attempt
+        self.assertIsNone(
+            cache.get(f"status_types_for_case_type:{self.zaaktype['url']}")
+        )
+        self.assertIsNone(
+            cache.get(f"status_types_for_case_type:{self.zaaktype['url']}")
+        )
+
+        self.app.get(self.url, user=self.user)
+
+        self.assertIsNotNone(
+            cache.get(f"status_types_for_case_type:{self.zaaktype['url']}")
+        )
+        self.assertIsNotNone(
+            cache.get(f"status_types_for_case_type:{self.zaaktype['url']}")
+        )
+
+        # Second attempt with new case and status type
+        m.get(
+            f"{ZAKEN_ROOT}zaken?rol__betrokkeneIdentificatie__natuurlijkPersoon__inpBsn=900222086",
+            json=paginated_response([self.zaak1, self.zaak2, self.new_zaak]),
+        )
+        m.get(
+            f"{CATALOGI_ROOT}zaaktypen/53340e34-7581-4b04-884f-98ui7y87i876",
+            json=self.new_zaaktype,
+        )
+        m.get(
+            f"{CATALOGI_ROOT}statustypen",
+            json=paginated_response(
+                [self.status_type1, self.status_type2, self.new_status_type]
+            ),
+        )
+        m.get(
+            f"{ZAKEN_ROOT}statussen/3da81560-c7fc-476a-ad13-oie8u899923g",
+            json=self.new_status,
+        )
+
+        self.assertIsNone(
+            cache.get(f"status_types_for_case_type:{self.new_zaaktype['url']}")
+        )
+
+        self.app.get(self.url, user=self.user)
+
+        self.assertIsNotNone(
+            cache.get(f"status_types_for_case_type:{self.new_zaaktype['url']}")
+        )
+        self.assertIsNotNone(
+            cache.get(f"status_types_for_case_type:{self.zaaktype['url']}")
+        )
+        self.assertIsNotNone(
+            cache.get(f"status_types_for_case_type:{self.zaaktype['url']}")
+        )
+
+    def test_statuses_are_cached(self, m):
+        self._setUpMocks(m)
+
+        # Cache is empty before the request
+        self.assertIsNone(cache.get(f"status:{self.status1['url']}"))
+        self.assertIsNone(cache.get(f"status:{self.status2['url']}"))
+
+        self.app.get(self.url, user=self.user)
+
+        # Status is cached after the request
+        self.assertIsNotNone(cache.get(f"status:{self.status1['url']}"))
+        self.assertIsNotNone(cache.get(f"status:{self.status2['url']}"))
+
+    def test_cached_statuses_are_deleted_after_one_hour(self, m):
+        self._setUpMocks(m)
+
+        with freeze_time("2022-01-01 12:00") as frozen_time:
+            self.app.get(self.url, user=self.user)
+
+            # After one hour the results should be deleted
+            frozen_time.tick(delta=datetime.timedelta(hours=1))
+            self.assertIsNone(cache.get(f"status:{self.status1['url']}"))
+            self.assertIsNone(cache.get(f"status:{self.status2['url']}"))
+
+    def test_cached_statuses_in_combination_with_new_ones(self, m):
+        self._setUpMocks(m)
+
+        # First attempt
+        self.assertIsNone(cache.get(f"status:{self.status1['url']}"))
+        self.assertIsNone(cache.get(f"status:{self.status2['url']}"))
+
+        self.app.get(self.url, user=self.user)
+
+        self.assertIsNotNone(cache.get(f"status:{self.status1['url']}"))
+        self.assertIsNotNone(cache.get(f"status:{self.status2['url']}"))
+
+        # Second attempt with new case and status type
+        m.get(
+            f"{ZAKEN_ROOT}zaken?rol__betrokkeneIdentificatie__natuurlijkPersoon__inpBsn=900222086",
+            json=paginated_response([self.zaak1, self.zaak2, self.new_zaak]),
+        )
+        m.get(
+            f"{CATALOGI_ROOT}zaaktypen/53340e34-7581-4b04-884f-98ui7y87i876",
+            json=self.new_zaaktype,
+        )
+        m.get(
+            f"{CATALOGI_ROOT}statustypen",
+            json=paginated_response(
+                [self.status_type1, self.status_type2, self.new_status_type]
+            ),
+        )
+        m.get(
+            f"{ZAKEN_ROOT}statussen/3da81560-c7fc-476a-ad13-oie8u899923g",
+            json=self.new_status,
+        )
+        m.get(
+            f"{CATALOGI_ROOT}statustypen/e3798107-ab27-4c3c-977d-984yr8887rhe",
+            json=self.new_status_type,
+        )
+
+        self.assertIsNone(cache.get(f"status:{self.new_status['url']}"))
+
+        self.app.get(self.url, user=self.user)
+
+        self.assertIsNotNone(cache.get(f"status:{self.new_status['url']}"))
+        self.assertIsNotNone(cache.get(f"status:{self.status1['url']}"))
+        self.assertIsNotNone(cache.get(f"status:{self.status2['url']}"))
+
+
+class ClosedCaseListCacheTests(OpenCaseListCacheTests):
+    """
+    run the same tests as for open cases
+    """
+
+    url = reverse_lazy("accounts:my_closed_cases")
+
+    @classmethod
+    def setUpTestData(cls):
+        super().setUpTestData()
+
+        for zaak in [cls.zaak1, cls.zaak2, cls.new_zaak]:
+            zaak["einddatum"] = "2022-01-16"
