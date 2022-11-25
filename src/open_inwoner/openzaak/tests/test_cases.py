2cc4f750e46b3463f2e18e08c74fc216b9917c1d
Anna Shamray <anna.shamray@maykinmedia.nl>
2022-11-25 14:51:19 +0100

    :truck: [#892] move fetch case tests to the separate file

--- src/open_inwoner/openzaak/tests/test_cases.py
+++ src/open_inwoner/openzaak/tests/test_cases.py
@@ -14,7 +14,6 @@ from zgw_consumers.test import generate_oas_component, mock_service_oas_get
 
 from open_inwoner.accounts.choices import LoginTypeChoices
 from open_inwoner.accounts.tests.factories import UserFactory
-from open_inwoner.openzaak.cases import fetch_single_case
 from open_inwoner.utils.test import paginated_response
 
 from ..models import OpenZaakConfig
@@ -756,61 +755,3 @@ class TestListCasesView(WebTest):
         self.assertEquals(response.status_code, 200)
         self.assertListEqual(response.context.get("open_cases"), [])
         self.assertListEqual(response.context.get("closed_cases"), [])
-
-
-@requests_mock.Mocker()
-class TestFetchSpecificCase(WebTest):
-    def setUp(self):
-        self.config = OpenZaakConfig.get_solo()
-        self.zaak_service = ServiceFactory(api_root=ZAKEN_ROOT, api_type=APITypes.zrc)
-        self.config.zaak_service = self.zaak_service
-        self.config.save()
-
-        self.zaak = generate_oas_component(
-            "zrc",
-            "schemas/Zaak",
-            url=f"{ZAKEN_ROOT}zaken/d8bbdeb7-770f-4ca9-b1ea-77b4730bf67d",
-            identificatie="ZAAK-2022-0000000024",
-            omschrijving="Zaak naar aanleiding van ingezonden formulier",
-            startdatum="2022-01-02",
-            einddatum=None,
-        )
-
-    def _setUpMocks(self, m):
-        mock_service_oas_get(m, ZAKEN_ROOT, "zrc")
-        m.get(
-            f"{ZAKEN_ROOT}zaken/d8bbdeb7-770f-4ca9-b1ea-77b4730bf67d",
-            json=self.zaak,
-        )
-
-    def test_case_is_retrieved(self, m):
-        self._setUpMocks(m)
-
-        case = fetch_single_case("d8bbdeb7-770f-4ca9-b1ea-77b4730bf67d")
-
-        self.assertEquals(
-            case.url,
-            f"{ZAKEN_ROOT}zaken/d8bbdeb7-770f-4ca9-b1ea-77b4730bf67d",
-        )
-
-    def test_no_case_is_retrieved_when_http_404(self, m):
-        mock_service_oas_get(m, ZAKEN_ROOT, "zrc")
-        m.get(
-            f"{ZAKEN_ROOT}zaken/d8bbdeb7-770f-4ca9-b1ea-77b4730bf67d",
-            status_code=404,
-        )
-
-        case = fetch_single_case("d8bbdeb7-770f-4ca9-b1ea-77b4730bf67d")
-
-        self.assertIsNone(case)
-
-    def test_no_case_is_retrieved_when_http_500(self, m):
-        mock_service_oas_get(m, ZAKEN_ROOT, "zrc")
-        m.get(
-            f"{ZAKEN_ROOT}zaken/d8bbdeb7-770f-4ca9-b1ea-77b4730bf67d",
-            status_code=500,
-        )
-
-        case = fetch_single_case("d8bbdeb7-770f-4ca9-b1ea-77b4730bf67d")
-
-        self.assertIsNone(case)
