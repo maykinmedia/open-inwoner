25afe67252007fecd9f03572b45d75accd8b7389
Sven van de Scheur <svenvandescheur@gmail.com>
2022-11-28 16:31:51 +0100

    :sparkles: - Use htmx to async update messages. (#342)
    * :sparkles: - Use htmx to async update messages.
    
    * :white_check_mark: - Add test for async_selector in messages.
    
    * :shirt: - Black.
    
    * :arrow_up: - Selenium.
    
    * :green_heart: - Make selenium/Firefox run headless.
    
    * :shirt: - isort
    
    * :green_heart: - Attempt to fix the build.
    
    * :white_check_mark: Uncomment commented out test.
    
    * :shirt: - prettier
    
    * :heavy_plus_sign: bump certifi and add selenium
    
    * :ok_hand: - action variable is not documented in template tags.
    
    * :ok_hand: - Should we also add polling for messages?
    
    * :ok_hand: - Why to use this tag? I commented it and nothing changed.
    
    * :ok_hand: - Nice! Perhaps in the future we can also test it for Chrome
    
    * :shirt: - Black and isort.
    
    * :ok_hand: - Should we make it 5 seconds?
    
    * :bug: - Fix bug in accessibility code.
    
    * :memo - Fix documentation.
    
    * :ok_hand: - Please add test for polling.
    
    * :shirt - black
    
    * :bug: - Fix incorrect argument name.
    
    Co-authored-by: Anna Shamray <anna.shamray@maykinmedia.nl>

--- src/open_inwoner/accounts/tests/test_inbox_page.py
+++ src/open_inwoner/accounts/tests/test_inbox_page.py
@@ -1,7 +1,13 @@
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from django.urls import reverse_lazy
 
 from django_webtest import WebTest
 from privates.test import temp_private_root
+from selenium.webdriver.chrome.options import Options as ChromeOptions
+from selenium.webdriver.chrome.webdriver import WebDriver as ChromeDriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.firefox.options import Options as FirefoxOptions
+from selenium.webdriver.firefox.webdriver import WebDriver as FirefoxDriver
 
 from ..models import Message
 from .factories import ContactFactory, MessageFactory, UserFactory
@@ -117,3 +123,140 @@ class InboxPageTests(WebTest):
 
         self.assertFalse(message_sent.seen)
         self.assertTrue(message_received.seen)
+
+
+class BaseInboxPageSeleniumTests:
+    options = None
+    driver = None
+    selenium = None
+
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.selenium.implicitly_wait(10)
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.selenium.quit()
+        super().tearDownClass()
+
+    def setUp(self):
+        self.me = UserFactory.create(
+            email="me@example.com", password="s3cret", is_staff=True
+        )
+        self.user1 = UserFactory.create(
+            first_name="user", last_name="1", email="user1@example.com"
+        )
+        self.user2 = UserFactory.create(
+            first_name="user", last_name="2", email="user2@example.com"
+        )
+        ContactFactory.create(
+            created_by=self.me, contact_user=self.user1, email=self.user1.email
+        )
+        ContactFactory.create(
+            created_by=self.me, contact_user=self.user2, email=self.user2.email
+        )
+        MessageFactory.create(sender=self.me, receiver=self.user1)
+        MessageFactory.create(receiver=self.me, sender=self.user2)
+
+    def tearDown(self):
+        self.selenium.delete_all_cookies()
+
+    def test_async_selector(self):
+        self.given_i_am_logged_in()
+        self.when_i_navigate_to_page()
+
+        # Send message.
+        message_count = len(self.selenium.find_elements(By.CSS_SELECTOR, ".message"))
+        content_textarea = self.selenium.find_element(By.NAME, "content")
+        content_textarea.send_keys("Lorem ipsum dolor sit amet.")
+        form = self.selenium.find_element(By.CSS_SELECTOR, "#message-form")
+        form.submit()
+
+        # Assert message.
+        selector = f".messages__list-item:nth-child({message_count + 1}) .message"
+        message = self.selenium.find_element(By.CSS_SELECTOR, selector)
+        self.assertIn("Lorem ipsum dolor sit amet.", message.text)
+
+        # assert async.
+        url = f"{self.live_server_url}{reverse_lazy('accounts:inbox')}?redirected=True"
+        self.assertEqual(url, self.selenium.current_url)
+        self.assertNotIn("#messages-last", self.selenium.current_url)
+
+    def test_polling(self):
+        self.given_i_am_logged_in()
+        self.when_i_navigate_to_page()
+
+        # Create message.
+        initial_message_count = len(
+            self.selenium.find_elements(By.CSS_SELECTOR, ".message")
+        )
+        initial_selector = (
+            f".messages__list-item:nth-child({initial_message_count}) .message"
+        )
+        initial_message = self.selenium.find_element(By.CSS_SELECTOR, initial_selector)
+        initial_text = initial_message.text
+
+        Message.objects.create(
+            receiver=self.me, sender=self.user2, content="Lorem ipsum dolor sit amet."
+        )
+
+        # Assert message.
+        new_selector = (
+            f".messages__list-item:nth-child({initial_message_count + 1}) .message"
+        )
+        new_message = self.selenium.find_element(By.CSS_SELECTOR, new_selector)
+        self.assertIn("Lorem ipsum dolor sit amet.", new_message.text)
+
+        # Previous message.
+        previous_selector = (
+            f".messages__list-item:nth-child({initial_message_count}) .message"
+        )
+        previous_message = self.selenium.find_element(
+            By.CSS_SELECTOR, previous_selector
+        )
+        self.assertEqual(initial_text, previous_message.text)
+
+        # assert async.
+        url = f"{self.live_server_url}{reverse_lazy('accounts:inbox')}?redirected=True"
+        self.assertEqual(url, self.selenium.current_url)
+        self.assertNotIn("#messages-last", self.selenium.current_url)
+
+    def given_i_am_logged_in(self):
+        self.selenium.get("%s%s" % (self.live_server_url, reverse_lazy("admin:login")))
+        username_input = self.selenium.find_element(By.NAME, "username")
+        username_input.send_keys("me@example.com")
+        password_input = self.selenium.find_element(By.NAME, "password")
+        password_input.send_keys("s3cret")
+        self.selenium.find_element(By.XPATH, '//input[@type="submit"]').click()
+
+    def when_i_navigate_to_page(self):
+        self.selenium.get(
+            "%s%s" % (self.live_server_url, reverse_lazy("accounts:inbox"))
+        )
+
+
+class InboxPageFirefoxSeleniumTests(
+    BaseInboxPageSeleniumTests, StaticLiveServerTestCase
+):
+    options = FirefoxOptions()
+    driver_class = FirefoxDriver
+
+    @classmethod
+    def setUpClass(cls):
+        cls.options.headless = True
+        cls.selenium = cls.driver_class(options=cls.options)
+        super().setUpClass()
+
+
+class InboxPageChromeSeleniumTests(
+    BaseInboxPageSeleniumTests, StaticLiveServerTestCase
+):
+    options = ChromeOptions()
+    driver_class = ChromeDriver
+
+    @classmethod
+    def setUpClass(cls):
+        cls.options.headless = True
+        cls.selenium = cls.driver_class(options=cls.options)
+        super().setUpClass()
