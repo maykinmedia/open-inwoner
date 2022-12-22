f0cfc0559d640314b1da967d2219f333f6f25406
vasileios <zigras00@gmail.com>
2022-12-22 10:29:36 +0100

    [#914] Added template tag for optional texts

--- src/open_inwoner/components/templatetags/string_tags.py
+++ src/open_inwoner/components/templatetags/string_tags.py
@@ -1,4 +1,6 @@
 from django import template
+from django.template.defaultfilters import linebreaksbr
+from django.utils.html import format_html
 
 register = template.Library()
 
@@ -7,3 +9,19 @@ register = template.Library()
 def addstr(arg1, arg2):
     """concatenate arg1 & arg2"""
     return str(arg1) + str(arg2)
+
+
+@register.simple_tag
+def optional_paragraph(field):
+    """
+    Renders the provided optional text, if it exists, or an empty string.
+
+    Usage:
+        {% optional_paragraph field=field %}
+
+    Variables:
+        + field: Field | This is the text field.
+    """
+    if not field:
+        return ""
+    return format_html('<p class="p">{field}</p>'.format(field=linebreaksbr(field)))
