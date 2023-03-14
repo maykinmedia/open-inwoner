1c859b867312a311450b3b791e581e27859d86e0
Jiro Ghianni <jiro@maykinmedia.nl>
2023-03-14 17:03:14 +0100

    :art: [#1180] Added cleaner eventlistener for less janky scroll

--- src/open_inwoner/js/components/anchor-menu/scroll_to_top.js
+++ src/open_inwoner/js/components/anchor-menu/scroll_to_top.js
@@ -2,14 +2,18 @@
 const mybutton = document.getElementById('scroll-anchor')
 
 // When the user scrolls down 80px from the top of the document, show the button
-window.onscroll = function () {
-  scrollFunction()
-}
-
-function scrollFunction() {
-  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-    mybutton.style.display = 'block'
-  } else {
-    mybutton.style.display = 'none'
-  }
-}
+document.addEventListener(
+  'scroll',
+  (e) => {
+    if (
+      document.body.scrollTop > 80 ||
+      document.documentElement.scrollTop > 80
+    ) {
+      mybutton.style.display = 'block'
+    } else {
+      mybutton.style.display = 'none'
+    }
+  },
+  // prevent scroll jank in modern browsers
+  { passive: true }
+)
