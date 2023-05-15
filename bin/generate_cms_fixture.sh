407e55e9fc85371d19bd233f30dab488adae8486
Alex de Landgraaf <alex@maykinmedia.nl>
2023-05-15 18:49:28 +0000

    Adding pageextension to fixture

--- bin/generate_cms_fixture.sh
+++ bin/generate_cms_fixture.sh
@@ -8,4 +8,8 @@
 #
 # Run this script from the root of the repository
 
-src/manage.py dumpdata --indent=4 --natural-foreign --natural-primary cms.treenode cms.placeholder cms.page cms.title cms.cmsplugin > src/open_inwoner/conf/fixtures/cms-page.json
+# Note: you will likely need to restart uwsgi/supervisor/docker afterwards for apphooks to be registered properly
+
+src/manage.py dumpdata --indent=4 --natural-primary cms.treenode cms.placeholder cms.page cms.title cms.cmsplugin extensions.commonextension > src/open_inwoner/conf/fixtures/cms-page.json
+
+
