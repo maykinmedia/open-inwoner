69d688c36a35a92d3320c3adfed7fcd480af8ab5
Paul Schilling <mail@paulschilling.de>
2023-11-10 17:03:27 +0100

    [#1830] Prevent deletion of last website

--- src/open_inwoner/configurations/tests/test_admin.py
+++ src/open_inwoner/configurations/tests/test_admin.py
@@ -1,11 +1,105 @@
+from django.contrib.sites.models import Site
 from django.urls import reverse
 from django.utils.translation import ugettext_lazy as _
 
 from django_webtest import WebTest
+from pyquery import PyQuery
 
 from open_inwoner.accounts.tests.factories import UserFactory
 
 from ..models import SiteConfiguration
+
+
+class TestAdminSite(WebTest):
+    csrf_checks = False
+
+    @classmethod
+    def setUpTestData(cls):
+        cls.user = UserFactory(is_superuser=True, is_staff=True)
+
+    def test_delete_site_success(self):
+        site2 = Site.objects.create(id=2, domain="example2.com", name="example2")
+
+        # delete site
+        response = self.app.get(
+            reverse("admin:sites_site_delete", kwargs={"object_id": site2.id}),
+            user=self.user,
+        )
+        response = response.form.submit().follow()
+
+        # check sites
+        sites = Site.objects.all()
+        self.assertEqual(len(sites), 1)
+
+        # check message
+        doc = PyQuery(response.content)
+
+        success_msg = doc.find(".success").text()
+        self.assertIn("met succes verwijderd.", success_msg)
+
+    def test_delete_last_site_fail(self):
+        # attempt to delete site
+        response = self.app.get(
+            reverse("admin:sites_site_delete", kwargs={"object_id": 1}), user=self.user
+        )
+        response = response.form.submit().follow()
+
+        # check sites
+        sites = Site.objects.all()
+        self.assertEqual(len(sites), 1)
+
+        # check message
+        doc = PyQuery(response.content)
+
+        warning_msg = doc.find(".warning").text()
+        self.assertEqual(warning_msg, "You cannot delete the last site.")
+
+    def test_bulk_delete_success(self):
+        site2 = Site.objects.create(id=2, domain="example2.com", name="example2")
+        site3 = Site.objects.create(id=3, domain="example3.com", name="example3")
+
+        # delete sites
+        data = {"action": "delete_selected", "_selected_action": [site2.pk, site3.pk]}
+        response = self.app.post(
+            reverse("admin:sites_site_changelist"), data, user=self.user
+        )
+        response = response.form.submit().follow()  # confirmation form
+
+        # check sites
+        sites = Site.objects.all()
+        self.assertEqual(len(sites), 1)
+
+        # check message
+        doc = PyQuery(response.content)
+
+        success_msg = doc.find(".success").text()
+        self.assertEqual(success_msg, "2 websites met succes verwijderd.")
+
+    def test_bulk_delete_fail(self):
+        Site.objects.create(id=2, domain="example2.com", name="example2")
+        Site.objects.create(id=3, domain="example3.com", name="example3")
+
+        # attempt to delete sites
+        data = {
+            "action": "delete_selected",
+            "_selected_action": [site.pk for site in Site.objects.all()],
+        }
+        response = self.app.post(
+            reverse("admin:sites_site_changelist"), data, user=self.user
+        ).follow()
+
+        # check sites
+        sites = Site.objects.all()
+        self.assertEqual(len(sites), 3)
+
+        # check message
+        doc = PyQuery(response.content)
+
+        warning_msg = doc.find(".warning").text()
+        self.assertEqual(
+            warning_msg,
+            "You cannot delete all websites; at least one site must remain.",
+        )
 
 
 class TestAdminForm(WebTest):
