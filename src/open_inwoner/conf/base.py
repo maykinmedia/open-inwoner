bbc1a2a2bd6ab0897c89378242bf8dbf674dafb0
Alex de Landgraaf <alex@maykinmedia.nl>
2023-12-01 18:21:58 +0100

    Bumping version for release

--- src/open_inwoner/conf/base.py
+++ src/open_inwoner/conf/base.py
@@ -652,7 +652,7 @@ HIJACK_ALLOW_GET_REQUESTS = True
 # SENTRY - error monitoring
 #
 SENTRY_DSN = config("SENTRY_DSN", None)
-RELEASE = "v1.11"  # get_current_version()
+RELEASE = "v1.12"  # get_current_version()
 
 PRIVATE_MEDIA_ROOT = os.path.join(BASE_DIR, "private_media")
 FILER_ROOT = os.path.join(BASE_DIR, "media", "filer")
