d30ac5f07d9ad61758aaef73436fac7e6d51d12e
Alex de Landgraaf <alex@maykinmedia.nl>
2023-12-01 12:14:08 +0100

    [#1279] Updating translations for sprint 17 + Mijn Aanvragen sprint

--- src/open_inwoner/configurations/tests/test_oidc.py
+++ src/open_inwoner/configurations/tests/test_oidc.py
@@ -54,7 +54,7 @@ class OIDCConfigTest(ClearCachesMixin, WebTest):
         link = response.pyquery.find("[title='Inloggen met Azure AD']")
         link_text = link.find(".link__text").text()
 
-        self.assertEqual(link_text, _(""))
+        self.assertEqual(link_text, "")
 
         # admin login
         response = self.client.get(reverse("admin:login"))
