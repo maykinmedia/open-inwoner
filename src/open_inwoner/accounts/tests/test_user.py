b9aad9d692613aac4aba8a265508b805034a949f
Paul Schilling <mail@paulschilling.de>
2024-01-22 16:35:31 +0100

    [#2035] Remove user birthday field

--- src/open_inwoner/accounts/tests/test_user.py
+++ src/open_inwoner/accounts/tests/test_user.py
@@ -43,30 +43,6 @@ class UserTests(TestCase):
         )
         self.assertEqual(user.get_full_name(), "")
 
-    @freeze_time("2021-07-07 12:00:00")
-    def test_get_age_same_day(self):
-        with freeze_time("1990-07-07"):
-            user = User(birthday=date.today())
-        self.assertEqual(user.get_age(), 31)
-
-    @freeze_time("2021-07-07 12:00:00")
-    def test_get_age_day_before(self):
-        with freeze_time("1990-07-08"):
-            user = User(birthday=date.today())
-        self.assertEqual(user.get_age(), 30)
-
-    @freeze_time("2021-07-07 12:00:00")
-    def test_get_age_day_after(self):
-        with freeze_time("1990-07-06"):
-            user = User(birthday=date.today())
-        self.assertEqual(user.get_age(), 31)
-
-    @freeze_time("2021-07-07 12:00:00")
-    def test_get_age_young(self):
-        with freeze_time("2014-01-07"):
-            user = User(birthday=date.today())
-        self.assertEqual(user.get_age(), 7)
-
     def test_require_necessary_fields(self):
         user = UserFactory()
         self.assertFalse(user.require_necessary_fields())
