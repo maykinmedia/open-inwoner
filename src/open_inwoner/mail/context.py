ff2616276608762ca9fa7e1d5789ca5da18776f6
Bart van der Schoor <bart@maykinmedia.nl>
2024-02-14 12:24:28 +0100

    [#2113] Added some urls to mail template context

--- src/open_inwoner/mail/context.py
+++ src/open_inwoner/mail/context.py
@@ -1,3 +1,6 @@
+from django.urls import reverse
+
+from open_inwoner.cms.utils.page_display import profile_page_is_published
 from open_inwoner.configurations.models import SiteConfiguration
 
 
@@ -16,4 +19,17 @@ def mail_context():
         "secondary_font_color": config.secondary_font_color,
         "accent_font_color": config.accent_font_color,
     }
+    context["login_page"] = reverse("login")
+
+    if profile_page_is_published():
+        context["profile_notifications"] = reverse("profile:notifications")
+        context["profile_page"] = reverse("profile:detail")
+    else:
+        # empty urls to homepage
+        context["profile_notifications"] = ""
+        context["profile_page"] = ""
+
+    # if a contact page exists it is a FlatPage
+    context["contactpage"] = "????"
+
     return context
