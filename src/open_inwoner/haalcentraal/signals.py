19b8e28ca45ea06de4673c0823af8ec7a7df718e
Bart van der Schoor <bart@maykinmedia.nl>
2024-02-29 12:45:27 +0100

    Fixed user save-signal issue with Haalcentraal BRP retrieval logging

--- src/open_inwoner/haalcentraal/signals.py
+++ src/open_inwoner/haalcentraal/signals.py
@@ -1,32 +1,26 @@
 import logging
 
-from django.db.models.signals import pre_save
+from django.db.models.signals import post_save
 from django.dispatch import receiver
 
 from open_inwoner.accounts.choices import LoginTypeChoices
 from open_inwoner.accounts.models import User
-from open_inwoner.utils.logentry import system_action
 
 from .utils import update_brp_data_in_db
 
 logger = logging.getLogger(__name__)
 
 
-@receiver(pre_save, sender=User)
+@receiver(post_save, sender=User)
 def on_bsn_change(instance, **kwargs):
     if (
         instance.bsn
-        and instance.is_prepopulated is False
+        and not instance.is_prepopulated
         and instance.login_type == LoginTypeChoices.digid
+        and getattr(instance, "_process_on_bsn_change_post_save", True)
     ):
-        try:
-            system_action(
-                "Retrieving data for %s from haal centraal based on BSN",
-                content_object=instance,
-            )
-        except ValueError:
-            logger.info(
-                "Retrieving data for %s from haal centraal based on BSN", instance
-            )
+        # workaround to not have a post_save-signal loop if we save() again from within this handler
+        # note: this used to be a pre_save, but we need a saved user for the timeline log of the BRP access
+        instance._process_on_bsn_change_post_save = False
 
         update_brp_data_in_db(instance)
