017f26150fb7eb56c2ed66b32425145b9ee5cac4
Steven Bal <steven@maykinmedia.nl>
2024-03-26 13:49:25 +0100

    :bug: [#2280] Properly catch errors when fetching openstaande taken
    issue: https://taiga.maykinmedia.nl/project/open-inwoner/issue/2280

--- src/open_inwoner/openzaak/clients.py
+++ src/open_inwoner/openzaak/clients.py
@@ -658,16 +658,12 @@ class FormClient(APIClient):
         if not bsn:
             return []
 
-        try:
-            response = self.get(
-                "openstaande-taken",
-                params={"bsn": bsn},
-            )
-            data = get_json_response(response)
-            all_data = list(pagination_helper(self, data))
-        except (RequestException, ClientError) as e:
-            logger.exception("exception while making request", exc_info=e)
-            return []
+        response = self.get(
+            "openstaande-taken",
+            params={"bsn": bsn},
+        )
+        data = get_json_response(response)
+        all_data = list(pagination_helper(self, data))
 
         results = factory(OpenTask, all_data)
 
