bfa0de73ca1dce6c07e64daf363bafee8bd42633
Jiro Ghianni <jiro@maykinmedia.nl>
2024-04-16 14:02:09 +0200

    :lipstick: [#2289] Adjusted newsletter section checkbox styles

--- src/open_inwoner/laposta/choices.py
+++ src/open_inwoner/laposta/choices.py
@@ -1,9 +1,9 @@
 from .api_models import LapostaList
 
 
-def get_list_choices(lists: list[LapostaList]) -> list[tuple[str, str]]:
+def get_list_choices(lists: list[LapostaList]) -> list[tuple[str, tuple[str, str]]]:
     return [
-        (laposta_list.list_id, f"{laposta_list.name}: {laposta_list.remarks}")
+        (laposta_list.list_id, (laposta_list.name, laposta_list.remarks))
         for laposta_list in lists
         if laposta_list.name
     ]
