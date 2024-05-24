e6443578ee9e3af2cd6a1e04c313e02063ce7c0d
Paul Schilling <mail@paulschilling.de>
2024-05-24 17:00:56 +0200

    [#2322] Fixed working dir in celery script

--- bin/start_celery.sh
+++ bin/start_celery.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-exec celery -A "src.open_inwoner" events -l info --camera django_celery_monitor.camera.Camera --frequency=2.0 &
-exec celery -A "src.open_inwoner" worker -E --max-tasks-per-child=50 -l info -B --scheduler django_celery_beat.schedulers:DatabaseScheduler
+exec celery --workdir src -A "open_inwoner" events -l info --camera django_celery_monitor.camera.Camera --frequency=2.0 &
+exec celery --workdir src -A "open_inwoner" worker -E --max-tasks-per-child=50 -l info -B --scheduler django_celery_beat.schedulers:DatabaseScheduler
