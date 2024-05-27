93473507ed257d78e640f55f368eb6eada636a99
Alex de Landgraaf <alex@maykinmedia.nl>
2024-05-27 11:24:30 +0200

    [#2322] Fix for deployment to auto-activate venv when starting celery

--- bin/start_celery.sh
+++ bin/start_celery.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
+. env/bin/activate
+
 exec celery --workdir src -A "open_inwoner" events -l info --camera django_celery_monitor.camera.Camera --frequency=2.0 &
 exec celery --workdir src -A "open_inwoner" worker -E --max-tasks-per-child=50 -l info -B --scheduler django_celery_beat.schedulers:DatabaseScheduler
