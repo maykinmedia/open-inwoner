9a7db36f9e447ea55586767bb105742763c13a13
Paul Schilling <mail@paulschilling.de>
2024-05-27 15:49:27 +0200

    [#2509] Create celery task to rebuild search index

---
+++ src/open_inwoner/search/tasks.py
@@ -0,0 +1,17 @@
+import logging
+
+from django.core.management import call_command
+from django.utils.translation import gettext as _
+
+from open_inwoner.celery import app
+
+logger = logging.getLogger(__name__)
+
+
+@app.task(name=_("Rebuild search index"))
+def rebuild_search_index():
+    logger.info("starting rebuild_search_index() task")
+
+    call_command("search_index", "--rebuild", "-f")
+
+    logger.info("finished rebuild_search_index() task")
