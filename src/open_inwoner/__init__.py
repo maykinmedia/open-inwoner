0a149f59f27cf9e3b866b2b928e8ce91ffd348f5
Bart van der Schoor <bart@maykinmedia.nl>
2024-05-28 12:19:07 +0200

    [#2510] Updated version number

--- src/open_inwoner/__init__.py
+++ src/open_inwoner/__init__.py
@@ -1,6 +1,6 @@
 from .celery import app as celery_app  # noqa
 
-__version__ = "1.16"
+__version__ = "1.16.5"
 __author__ = "Maykin Media"
 __homepage__ = "https://github.com/maykinmedia/open-inwoner"
 __all__ = ("celery_app",)
