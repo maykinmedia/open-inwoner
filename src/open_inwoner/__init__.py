4ad7cbfc3dddeabf688d9494b0339c5fee196eef
Bart van der Schoor <bart@maykinmedia.nl>
2024-05-28 14:01:26 +0200

    [#2510] Use shared version number

--- src/open_inwoner/__init__.py
+++ src/open_inwoner/__init__.py
@@ -1,6 +1,8 @@
+from django.conf import settings
+
 from .celery import app as celery_app  # noqa
 
-__version__ = "1.16.5"
+__version__ = settings.RELEASE
 __author__ = "Maykin Media"
 __homepage__ = "https://github.com/maykinmedia/open-inwoner"
 __all__ = ("celery_app",)
