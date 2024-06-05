c377a62b8efca3d4782c58e6db9b5a1cc26fe5dc
Sidney Richards <sidney@maykinmedia.nl>
2024-06-05 10:59:20 +0200

    [#2529] Add a RO admin mixin

--- src/open_inwoner/utils/admin.py
+++ src/open_inwoner/utils/admin.py
@@ -183,3 +183,14 @@ class PeriodicTaskAdmin(_PeriodicTaskAdmin):
 
 admin.site.unregister(PeriodicTask)
 admin.site.register(PeriodicTask, PeriodicTaskAdmin)
+
+
+class ReadOnlyAdminMixin:
+    def has_add_permission(self, request):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+    def has_change_permission(self, request, obj=None):
+        return False
