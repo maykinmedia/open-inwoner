b09c9ebb1c8d30f31b95b886bbad94e19d7d0ff8
Sidney Richards <sidney@maykinmedia.nl>
2024-06-20 16:23:27 +0200

    [#2537] Introduce a proxy class to invoke multiple ZGW clients in parallel

--- src/open_inwoner/openzaak/exceptions.py
+++ src/open_inwoner/openzaak/exceptions.py
@@ -1,3 +1,6 @@
+from typing import Sequence
+
+
 class InvalidAuth(Exception):
     pass
 
@@ -12,3 +15,18 @@ class InvalidAuthForClientID(InvalidAuth):
     def __init__(self, client_id):
         self.client_id = client_id
         super().__init__(f"secret invalid for subscription client_id'{client_id}'")
+
+
+class MultiZgwClientProxyError(ExceptionGroup):
+    """A container for exceptions raise within individual client requests."""
+
+    def __new__(cls, exceptions: Sequence[Exception]):
+        self = super().__new__(
+            MultiZgwClientProxyError,
+            "One of more ZGW clients raised an error",
+            exceptions,
+        )
+        return self
+
+    def derive(self, exceptions: Sequence[Exception]):
+        return MultiZgwClientProxyError(exceptions)
