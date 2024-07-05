748d8b8c159a7c62825ee69110f707de557b8d74
Sergei Maertens <sergei@maykinmedia.nl>
2024-07-06 00:22:47 +0200

    Hack around app label rename
    Probably the shadiest/trickest part of this upgrade - we have to keep
    existing migration history in mind to avoid running them twice (as
    those would crash out), but we must assume this SQL will also run
    when the new dependencies are set up and that would incorrectly mark
    the external package migrations as belonging to the legacy app.
    
    Therefore, we strictly limit the migrations that may be renamed to the
    ones that are known - the timestamps added by django definitely help
    here.
    
    The digid-eherkenning.oidc migrations also start with a squashed
    migration, so there is no collision on the 0001_initial migration :tada:

--- src/digid_eherkenning_oidc_generics/apps.py
+++ src/digid_eherkenning_oidc_generics/apps.py
@@ -1,4 +1,7 @@
+from contextlib import suppress
+
 from django.apps import AppConfig
+from django.db import OperationalError, ProgrammingError, connection
 from django.utils.translation import gettext_lazy as _
 
 
@@ -6,3 +9,28 @@ class DigiDeHerkenningOIDCAppConfig(AppConfig):
     name = "digid_eherkenning_oidc_generics"
     verbose_name = _("DigiD & eHerkenning via OpenID Connect")
     label = "digid_eherkenning_oidc_generics_legacy"
+
+    def ready(self):
+        # Hook into the app registry to update the django_migrations table
+        # *before* we run migrations.
+        with suppress(OperationalError, ProgrammingError):
+            rename_app_label()
+
+
+def rename_app_label():
+    with connection.cursor() as cursor:
+        # uses explicit query names so that we don't accidentally rwrite the
+        # new digid_eherkenning.oidc migrations
+        query = """
+            UPDATE django_migrations
+            SET app = 'digid_eherkenning_oidc_generics_legacy'
+            WHERE app = 'digid_eherkenning_oidc_generics'
+            AND (
+                name = '0001_initial'
+                OR name = '0002_auto_20240109_1055'
+                OR name = '0003_alter_openidconnectdigidconfig_oidc_exempt_urls_and_more'
+                OR name = '0004_alter_openidconnectdigidconfig_table_and_more'
+                OR name = '0001_legacy'
+            )
+        """
+        cursor.execute(query)
