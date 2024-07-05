3eaf384c15b1394d7d7f6b3a6218566be9d75b4e
Sergei Maertens <sergei@maykinmedia.nl>
2024-07-05 23:36:43 +0200

    Set up squashed migration and remove the 'fluff'
    Removed caching mixin from bases, as it's not relevant for
    the DB table structure and will make upgrading easier

--- src/digid_eherkenning_oidc_generics/apps.py
+++ src/digid_eherkenning_oidc_generics/apps.py
@@ -5,3 +5,4 @@ from django.utils.translation import gettext_lazy as _
 class DigiDeHerkenningOIDCAppConfig(AppConfig):
     name = "digid_eherkenning_oidc_generics"
     verbose_name = _("DigiD & eHerkenning via OpenID Connect")
+    # label = "digid_eherkenning_oidc_generics_legacy"
