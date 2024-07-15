363f0db4219ec66cab63b43cba528ce188fd3068
Sidney Richards <sidney@maykinmedia.nl>
2024-07-15 13:01:12 +0200

    [#2627] Verify that admin-index fixure can be (auto)loaded

--- src/open_inwoner/accounts/tests/test_admin_index_fixture.py
+++ src/open_inwoner/accounts/tests/test_admin_index_fixture.py
@@ -1,8 +1,11 @@
 from django.core.management import call_command
-from django.test import TestCase
+from django.db.models.signals import post_migrate
+from django.test import TestCase, TransactionTestCase
+
+from open_inwoner.accounts.apps import update_admin_index
 
 
-class DjangoAdminIndexFixtureTestCase(TestCase):
+class ManualLoadDjangoAdminIndexFixtureTestCase(TransactionTestCase):
     """This test case serves as a trigger to warn us when we forget to
     update the django-admin-index fixture."""
 
@@ -14,3 +17,12 @@ class DjangoAdminIndexFixtureTestCase(TestCase):
                 "Failed to load the django-admin-index fixture: perhaps you forgot to update them? "
                 f"Got error: {str(e)}"
             )
+
+
+class AutoLoadDjangoAdminIndexFixtureTestCase(TestCase):
+    def test_update_admin_index_hook_is_registered(self):
+        connected_functions = [receiver[1]() for receiver in post_migrate.receivers]
+        self.assertIn(update_admin_index, connected_functions)
+
+    def test_update_admin_index_hook_returns_true(self):
+        self.assertTrue(update_admin_index())
