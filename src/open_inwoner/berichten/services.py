c7178827dfbb3ad928d907fddb8beabec616ea96
Sidney Richards <sidney@maykinmedia.nl>
2024-10-09 13:00:21 +0200

    Update bericht opened status upon accessing detail page

--- src/open_inwoner/berichten/services.py
+++ src/open_inwoner/berichten/services.py
@@ -1,3 +1,4 @@
+import datetime
 from typing import Literal
 
 from objectsapiclient.client import Client as ObjectenClient
@@ -39,3 +40,27 @@ class BerichtenService:
         obj = self.client.get_object(uuid)
 
         return Bericht.model_validate(obj.record["data"] | {"object_uuid": obj.uuid})
+
+    def update_object(self, uuid: str, updated_data: dict):
+
+        # TODO: the PATCH method in the Objects API does not appear to work as
+        # expected. It validates the partial against the JSON Schema, so in this
+        # case you have to supply an object that is valid according to the
+        # schema. We thus have to do our own merging.
+
+        # Also: we are usign the underlying API directly to avoid going back and
+        # forth between camel and snake case.
+        existing_obj = self.client.objects_api.retrieve("object", uuid=uuid)
+        existing_data = existing_obj["record"]["data"]
+        self.client.objects_api.partial_update(
+            "object",
+            {
+                "record": {
+                    "startAt": datetime.date.today().isoformat(),
+                    "data": existing_data | updated_data,
+                }
+            },
+            uuid=uuid,
+        )
+        # Refresh the object and build the Bericht model
+        return self.client.get_object(uuid)
