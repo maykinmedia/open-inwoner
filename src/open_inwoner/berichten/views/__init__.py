e466df9617d10167d8af10491ab92832191b5014
Sidney Richards <sidney@maykinmedia.nl>
2024-10-09 17:10:40 +0200

    Add a URL to mark a bericht as unread

--- src/open_inwoner/berichten/views/__init__.py
+++ src/open_inwoner/berichten/views/__init__.py
@@ -1,4 +1,4 @@
-from .bericht_detail import BerichtDetailView
+from .bericht_detail import BerichtDetailView, mark_bericht_as_unread
 from .bericht_list import BerichtListView
 
-__all__ = ["BerichtDetailView", "BerichtListView"]
+__all__ = ["BerichtDetailView", "BerichtListView", "mark_bericht_as_unread"]
