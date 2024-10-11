707ecf51eb8989ee14d7d6019b3ce5e75ce2a63e
Sidney Richards <sidney@maykinmedia.nl>
2024-10-11 14:42:44 +0200

    Add bericht download functionality

--- src/open_inwoner/berichten/urls.py
+++ src/open_inwoner/berichten/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 
+from open_inwoner.berichten.views.bericht_detail import BerichtDownloadView
+
 from .views import BerichtDetailView, BerichtListView, MarkBerichtUnreadView
 
 app_name = "berichten"
@@ -10,6 +12,11 @@ urlpatterns = [
         "<uuid:object_uuid>/mark-unread",
         MarkBerichtUnreadView.as_view(),
         name="mark-bericht-unread",
+    ),
+    path(
+        "<uuid:object_uuid>/download",
+        BerichtDownloadView.as_view(),
+        name="download-bericht-attachment",
     ),
     path("", BerichtListView.as_view(), name="list"),
 ]
