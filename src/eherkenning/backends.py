6a257a1624bdc054c365054a75ce4e65468099f1
Sidney Richards <sidney@maykinmedia.nl>
2025-03-05 12:47:17 +0100

    [#3096] Add a vestiging field to the User model
    This commit adds a `vestiging` attribute to the User model.
    Vestigingen are for most use-cases, and in the eyes of most
    clients, distinct users from the legal entity to which
    the vestiging belongs. To handle this properly, we'll have
    to make vestigingen first-class citizens, so we can e.g. store
    profile information on a vestiging basis, and generally allow
    vestigingen to interact with OIP on a full-fledged basis (e.g.
    also having, for instance, different notification settings
    then the main legal entity).

--- src/eherkenning/backends.py
+++ src/eherkenning/backends.py
@@ -31,7 +31,7 @@ class eHerkenningBackend(_eHerkenningBackend):
         try:
             user = UserModel.eherkenning_objects.get_by_kvk(kvk)
         except UserModel.DoesNotExist:
-            user = UserModel.eherkenning_objects.eherkenning_create(kvk)
+            user = UserModel.eherkenning_objects.create(kvk=kvk)
             created = True
 
         if vestigingsnummer := self.get_company_branch_number(saml_attributes):
