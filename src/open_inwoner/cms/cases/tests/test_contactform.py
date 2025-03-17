50a8099c3ce053cc6c6154a44547bc1d84ac5275
Sidney Richards <sidney@maykinmedia.nl>
2025-03-17 18:01:27 +0100

    [#3096] Tag tests failing due to vestigingen attribute
    This commit tags tests currently failing due to the tighter
    constraints around the kvk and vestiging attribute on the User
    model. We'll work our way through these tags and amend them to
    pass as we move through the feature.

--- src/open_inwoner/cms/cases/tests/test_contactform.py
+++ src/open_inwoner/cms/cases/tests/test_contactform.py
@@ -4,7 +4,7 @@ from unittest.mock import ANY, patch
 from django.conf import settings
 from django.contrib.auth import signals
 from django.core import mail
-from django.test import Client, TestCase, override_settings
+from django.test import Client, TestCase, override_settings, tag
 from django.urls import reverse
 from django.utils.translation import gettext as _
 
@@ -703,6 +703,7 @@ class CasesContactFormTestCase(AssertMockMatchersMixin, ClearCachesMixin, WebTes
 
         mock_send_confirm.assert_called_once_with("foo@example.com", ANY)
 
+    @tag("user_model_with_vestiging")
     def test_form_success_with_api_eherkenning_user(
         self, m, mock_contactmoment, mock_send_confirm
     ):
