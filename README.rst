8b520fff61538f9f43421586cf7db85b77663eec
Sidney Richards <sidney@maykinmedia.nl>
2025-03-20 12:41:19 +0100

    Release v1.28.1

--- README.rst
+++ README.rst
@@ -3,7 +3,7 @@ Open Inwoner
 ==================
 
 
-:Version: 1.28.0
+:Version: 1.28.1
 :Demo: https://openinwoner.nl
 :Source: https://github.com/maykinmedia/open-inwoner
 :Documentation: https://docs.openinwoner.nl
--- src/open_inwoner/conf/base.py
+++ src/open_inwoner/conf/base.py
@@ -769,7 +769,7 @@ CELERY_WORKER_PREFETCH_MULTIPLIER = 1
 # SENTRY - error monitoring
 #
 SENTRY_DSN = config("SENTRY_DSN", None)
-RELEASE = "v1.28.0"  # get_current_version()
+RELEASE = "v1.28.1"  # get_current_version()
 
 PRIVATE_MEDIA_ROOT = os.path.join(BASE_DIR, "private_media")
 FILER_ROOT = os.path.join(BASE_DIR, "media", "filer")
