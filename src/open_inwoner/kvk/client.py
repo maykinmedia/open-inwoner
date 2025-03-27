650eec7aeb0790789e112ffa2e5015b8cd494c09
Sidney Richards <sidney@maykinmedia.nl>
2025-03-27 15:01:49 +0100

    Merge on_kvk_change into account post_login sync
    This commit consolidates the RSIN updating logic for the
    post_login signal in the accounts module. Apart from being
    cleaner (keeping user data synchronized is an accounts module
    concern, not a kvk module concern), it also ensures all the
    various eHerkenning user updates happen _before_ any pushes
    to the klanten APIs take place. This was not guaranteed to
    happen when the sync functions were split up, causing issues
    in the klanten API syncs which, depending on the config,
    might require the RSIN to be used to identify klanten.

--- src/open_inwoner/kvk/client.py
+++ src/open_inwoner/kvk/client.py
@@ -138,12 +138,8 @@ class KvKClient:
     def get_basisprofiel(self, kvk: str) -> dict:
         return self._request(f"{self.basisprofielen_endpoint}/{kvk}")
 
-    def retrieve_rsin_with_kvk(self, kvk, **kwargs) -> str | None:
-        basisprofiel = self._request(
-            f"{self.basisprofielen_endpoint}/{kvk}", params=kwargs
-        )
-
-        if not basisprofiel:
+    def retrieve_rsin_with_kvk(self, kvk: str) -> str | None:
+        if not (basisprofiel := self.get_basisprofiel(kvk)):
             return None
 
         try:
