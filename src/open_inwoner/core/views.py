0c7192c2130190576dcccbdb1c59ee0bb618b64a
Jiro Ghianni <118177951+jiromaykin@users.noreply.github.com>
2025-05-19 15:07:49 +0200

    [#3257] Updated NLDS packages, added NLDS unordered list.

--- src/open_inwoner/core/views.py
+++ src/open_inwoner/core/views.py
@@ -1,4 +1,7 @@
+from urllib.parse import urlencode
+
 from django.shortcuts import render
+from django.urls import reverse
 
 from open_inwoner.accounts.models import User
 from open_inwoner.cms.utils.page_display import (
@@ -48,14 +51,14 @@ def sitemap(request):
         )
     if case_page_is_published():
         context["cms_pages"].append(
-            {"url_name": "cases:index", "link_text": "Mijn aanvragen"}
+            {"url_name": "cases:index", "link_text": "Mijn zaken"}
         )
         context["cms_pages"].append(
             {"url_name": "cases:contactmoment_list", "link_text": "Mijn vragen"}
         )
     if collaborate_page_is_published():
         context["cms_pages"].append(
-            {"url_name": "collaborate:plan_list", "link_text": "Samenwerken"}
+            {"url_name": "collaborate:plan_list", "link_text": "Mijn samenwerkingen"}
         )
     if inbox_page_is_published():
         context["cms_pages"].append(
@@ -66,20 +69,36 @@ def sitemap(request):
             {"url_name": "products:questionnaire_list", "link_text": "Zelftest"}
         )
     if profile_page_is_published():
+        base_url = reverse("profile:contact_list")
+        begeleiders_url = f"{base_url}?{urlencode({'type': 'begeleider'})}"
+
         context["cms_profile_pages"] = [
-            {"url_name": "profile:detail", "link_text": "Mijn Profiel"},
-            {"url_name": "profile:categories", "link_text": "Mijn interessegebieden"},
-            {"url_name": "profile:contact_list", "link_text": "Mijn begeleiders"},
-            {"url_name": "profile:action_list", "link_text": "Mijn contacten"},
+            {"url_name": "profile:detail", "link_text": "Mijn profiel"},
+            {
+                "url_name": "profile:categories",
+                "link_text": "Mijn interessegebieden",
+            },
+            {
+                "url": begeleiders_url,
+                "link_text": "Mijn begeleiders",
+            },
+            {"url_name": "profile:contact_list", "link_text": "Mijn contacten"},
             {"url_name": "profile:appointments", "link_text": "Mijn afspraken"},
+            {"url_name": "profile:action_list", "link_text": "Openstaande acties"},
         ]
 
-    # add "platform pages" (login/create account, cookie info etc.)
+    # add "platform pages" (conditional login/register page, cookie info etc.)
     context["platform_pages"] = [
         {"url_name": "pages-root", "link_text": "Home page"},
-        {"url_name": "login", "link_text": "Login or create an account"},
         {"url_name": "contactform", "link_text": "Contactformulier"},
     ]
+
+    # hide login links for users that are logged in
+    if not request.user.is_authenticated:
+        context["platform_pages"].append(
+            {"url_name": "login", "link_text": "Login or create an account"}
+        )
+
     # add flatpages to platform_pages
     config = SiteConfiguration.get_solo()
     flatpages = (
