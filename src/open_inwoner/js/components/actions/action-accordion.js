008d976c41708e40e26c69a22029124c03a8e1d6
Stefan <stefan@maykinmedia.nl>
2025-05-28 12:05:04 +0200

    ✨ #3277 Implement new design for actions.

---
+++ src/open_inwoner/js/components/actions/action-accordion.js
@@ -0,0 +1,32 @@
+const ACTIONS = document.querySelectorAll('.action')
+
+class ActionAccordion {
+  /**
+   *
+   * @param {HTMLDivElement} node
+   */
+  constructor(node) {
+    this.node = node
+    this.bindEvents()
+  }
+
+  bindEvents() {
+    this.toggleButton.addEventListener('click', this.toggleAccordion.bind(this))
+  }
+
+  toggleAccordion() {
+    this.toggleButton.setAttribute('aria-expanded', !this.isExpaned)
+    this.node.setAttribute('aria-expanded', !this.isExpaned)
+  }
+
+  get isExpaned() {
+    return this.node.getAttribute('aria-expanded') === 'true'
+  }
+
+  get toggleButton() {
+    return this.node.querySelector('.action__header-button')
+  }
+}
+
+// Start!
+;[...ACTIONS].forEach((node) => new ActionAccordion(node))
