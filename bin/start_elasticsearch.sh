335e275399d136a0bdeaf4e6ee54bb03dab87337
Sidney Richards <sidney@maykinmedia.nl>
2025-06-30 15:50:44 +0200

    feat: upgrade Elasticsearch version and libraries to 9.x/8.x
    This commit bumps the elasticsearch libraries to version 8.x,
    which also supports ES server 9.x, and pins the ES image on
    the same bitnami version used in our Helm charts and openinwoner-
    deployment.
    
    Co-authored-by: linssen814 <thomas@maykinmedia.nl>
    Co-authored-by: swrichards <sidney@maykinmedia.nl>

--- bin/start_elasticsearch.sh
+++ bin/start_elasticsearch.sh
@@ -1,2 +1,11 @@
-docker pull docker.elastic.co/elasticsearch/elasticsearch:7.15.2
-docker run -p 9200:9200 -p 9300:9300 -e "discovery.type=single-node" -m=1g docker.elastic.co/elasticsearch/elasticsearch:7.15.2
+#!/bin/bash
+
+ES_IMAGE=bitnami/elasticsearch:9.0.3-debian-12-r1
+
+docker pull $ES_IMAGE
+docker run --rm \
+    -p 9200:9200 -p 9300:9300 \
+    -e "discovery.type=single-node" \
+    -e "ES_JAVA_OPTS=-Xms512m -Xmx512m" \
+    -m=1g \
+    $ES_IMAGE
