ccfc3bba7e72304b4bb0331270f8009fb1b77da7
Paul Schilling <paul@maykinmedia.nl>
2025-06-30 11:12:02 +0100

    fix: only show CMS pages in sitemap if enabled via AppHookConfig

--- src/open_inwoner/core/views.py
+++ src/open_inwoner/core/views.py
@@ -1,3 +1,4 @@
+import logging
 from urllib.parse import urlencode
 
 from django.shortcuts import render
@@ -6,6 +7,7 @@ from django.urls import reverse
 from cms.models import Title
 
 from open_inwoner.accounts.models import User
+from open_inwoner.cms.profile.cms_appconfig import ProfileConfig
 from open_inwoner.cms.utils.page_display import (
     benefits_page_is_published,
     case_page_is_published,
@@ -15,7 +17,11 @@ from open_inwoner.cms.utils.page_display import (
     profile_page_is_published,
 )
 from open_inwoner.configurations.models import SiteConfiguration
+from open_inwoner.openklant.models import KlantenSysteemConfig
 from open_inwoner.pdc.models.category import Category
+from open_inwoner.questionnaire.models import QuestionnaireStep
+
+logger = logging.getLogger(__name__)
 
 
 def _get_category_data_for_user(cat: Category, user: User) -> dict:
@@ -71,29 +77,99 @@ def sitemap(request):
             {"url_name": "products:questionnaire_list", "link_text": "Zelftest"}
         )
     if profile_page_is_published():
-        base_url = reverse("profile:contact_list")
-        begeleiders_url = f"{base_url}?{urlencode({'type': 'begeleider'})}"
-
         context["cms_profile_pages"] = [
             {"url_name": "profile:detail", "link_text": "Mijn profiel"},
-            {
-                "url_name": "profile:categories",
-                "link_text": "Mijn interessegebieden",
-            },
-            {
-                "url": begeleiders_url,
-                "link_text": "Mijn begeleiders",
-            },
-            {"url_name": "profile:contact_list", "link_text": "Mijn contacten"},
-            {"url_name": "profile:appointments", "link_text": "Mijn afspraken"},
-            {"url_name": "profile:action_list", "link_text": "Openstaande acties"},
         ]
+
+        # conditionally add cms pages based on apphook configuration
+        cms_pages = context["cms_profile_pages"]
+        try:
+            profile_config = ProfileConfig.objects.get()
+        except ProfileConfig.MultipleObjectsReturned:
+            logger.warning("Multiple instances of ProfileConfig apphook found")
+            profile_config = ProfileConfig.objects.first()
+        if profile_config.selected_categories:
+            cms_pages.append(
+                {
+                    "url_name": "profile:categories",
+                    "link_text": "Mijn Interessegebieden",
+                },
+            )
+        if profile_config.mentors:
+            base_url = reverse("profile:contact_list")
+            begeleiders_url = f"{base_url}?{urlencode({'type': 'begeleider'})}"
+            cms_pages.append(
+                {
+                    "url": begeleiders_url,
+                    "link_text": "Mijn begeleiders",
+                },
+            )
+        if profile_config.my_contacts:
+            cms_pages.append(
+                {
+                    "url_name": "profile:contact_list",
+                    "link_text": "Mijn contacten",
+                },
+            )
+        if (
+            profile_config.selfdiagnose
+            and QuestionnaireStep.objects.filter(published=True).exists()
+        ):
+            cms_pages.append(
+                {
+                    "url_name": "products:questionnaire_list",
+                    "link_text": "Zelfdiagnose",
+                }
+            )
+        if profile_config.actions:
+            cms_pages.append(
+                {
+                    "url_name": "profile:action_list",
+                    "link_text": "Openstaande acties",
+                },
+            )
+        if profile_config.notifications:
+            cms_pages.append(
+                {
+                    "url_name": "profile:notifications",
+                    "link_text": "Notificatievoorkeuren",
+                }
+            )
+        if profile_config.questions:
+            cms_pages.append(
+                {
+                    "url_name": "cases:contactmoment_list",
+                    "link_text": "Mijn vragen",
+                }
+            )
+        if profile_config.ssd:
+            cms_pages.append(
+                {
+                    "url_name": "ssd:uitkeringen",
+                    "link_text": "Mijn uitkeringen",
+                }
+            )
+        if profile_config.appointments:
+            cms_pages.append(
+                {
+                    "url_name": "profile:appointments",
+                    "link_text": "Mijn afspraken",
+                },
+            )
 
     # add "platform pages" (conditional login/register page, cookie info etc.)
     context["platform_pages"] = [
         {"url_name": "pages-root", "link_text": "Home page"},
-        {"url_name": "contactform", "link_text": "Contactformulier"},
     ]
+
+    klant_config = KlantenSysteemConfig.get_solo()
+    if klant_config.has_contactform_configuration:
+        context["platform_pages"].append(
+            {
+                "url_name": "contactform",
+                "link_text": "Contactformulier",
+            },
+        )
 
     # hide login links for users that are logged in
     if not request.user.is_authenticated:
