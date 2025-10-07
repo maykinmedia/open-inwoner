5d48f7a8700e29b81604f744322a54b38f3ce57a
Sidney Richards <sidney@maykinmedia.nl>
2025-10-07 13:37:48 +0200

    chore: release v1.34.2

--- src/open_inwoner/conf/base.py
+++ src/open_inwoner/conf/base.py
@@ -776,7 +776,7 @@ CELERY_WORKER_PREFETCH_MULTIPLIER = 1
 # SENTRY - error monitoring
 #
 SENTRY_DSN = config("SENTRY_DSN", None)
-RELEASE = "v1.34.1"  # get_current_version()
+RELEASE = "v1.34.2"  # get_current_version()
 
 PRIVATE_MEDIA_ROOT = os.path.join(BASE_DIR, "private_media")
 FILER_ROOT = os.path.join(BASE_DIR, "media", "filer")
