af067177a428aed9e9e6904c01926b1ad5d9d924
Paul Schilling <paul@maykinmedia.nl>
2025-10-15 11:00:35 +0100

    chore: add and configure structlog
    Added and configured django-structlog, a wrapper around structlog
    for Django
    
    Refs: https://taiga.maykinmedia.nl/project/open-inwoner/us/3508

--- src/open_inwoner/celery.py
+++ src/open_inwoner/celery.py
@@ -1,9 +1,14 @@
+import logging
+import logging.config
 from pathlib import Path
 
 from django.conf import settings
 
+import structlog
 from celery import Celery, bootsteps
 from celery.signals import setup_logging, worker_ready, worker_shutdown
+
+from open_inwoner.conf.utils import config
 
 from .setup import setup_env
 
@@ -23,11 +28,75 @@ app.autodiscover_tasks()
 
 
 # Use django's logging settings as these are reset by Celery by default
-@setup_logging.connect()
-def config_loggers(*args, **kwargs):
-    from logging.config import dictConfig
+@setup_logging.connect
+def receiver_setup_logging(loglevel, logfile, format, colorize, **kwargs):
+    formatter = config("LOG_FORMAT_CONSOLE", default="json")
+    logging.config.dictConfig(
+        {
+            "version": 1,
+            "disable_existing_loggers": False,
+            "formatters": {
+                "json": {
+                    "()": structlog.stdlib.ProcessorFormatter,
+                    "processor": structlog.processors.JSONRenderer(),
+                    "foreign_pre_chain": [
+                        structlog.contextvars.merge_contextvars,
+                        structlog.processors.TimeStamper(fmt="iso"),
+                        structlog.stdlib.add_logger_name,
+                        structlog.stdlib.add_log_level,
+                        structlog.stdlib.PositionalArgumentsFormatter(),
+                    ],
+                },
+                "plain_console": {
+                    "()": structlog.stdlib.ProcessorFormatter,
+                    "processor": structlog.dev.ConsoleRenderer(),
+                    "foreign_pre_chain": [
+                        structlog.contextvars.merge_contextvars,
+                        structlog.processors.TimeStamper(fmt="iso"),
+                        structlog.stdlib.add_logger_name,
+                        structlog.stdlib.add_log_level,
+                        structlog.stdlib.PositionalArgumentsFormatter(),
+                    ],
+                },
+            },
+            "handlers": {
+                "console": {
+                    "class": "logging.StreamHandler",
+                    "formatter": formatter,
+                },
+            },
+            "loggers": {
+                "open_inwoner": {
+                    "handlers": ["console"],
+                    "level": "INFO",
+                },
+                "django_structlog": {
+                    "handlers": ["console"],
+                    "level": "INFO",
+                },
+            },
+        }
+    )
 
-    dictConfig(settings.LOGGING)
+    exception_processors = (
+        [structlog.processors.format_exc_info] if formatter == "json" else []
+    )
+    structlog.configure(
+        processors=[
+            structlog.contextvars.merge_contextvars,
+            structlog.stdlib.filter_by_level,
+            structlog.processors.TimeStamper(fmt="iso"),
+            structlog.stdlib.add_logger_name,
+            structlog.stdlib.add_log_level,
+            structlog.stdlib.PositionalArgumentsFormatter(),
+            structlog.processors.StackInfoRenderer(),
+            *exception_processors,
+            structlog.processors.UnicodeDecoder(),
+            structlog.stdlib.ProcessorFormatter.wrap_for_formatter,
+        ],
+        logger_factory=structlog.stdlib.LoggerFactory(),
+        cache_logger_on_first_use=True,
+    )
 
 
 HEARTBEAT_FILE = Path(settings.BASE_DIR) / "tmp" / "celery_worker_heartbeat"
