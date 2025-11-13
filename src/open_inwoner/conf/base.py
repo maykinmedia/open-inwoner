0c7da46db8bcf7caa806801191e32b1b89909cee
Sidney Richards <sidney@maykinmedia.nl>
2025-11-13 14:24:23 +0100

    chore: amend version to reflect dev target

--- src/open_inwoner/conf/base.py
+++ src/open_inwoner/conf/base.py
@@ -841,7 +841,7 @@ CELERY_WORKER_PREFETCH_MULTIPLIER = 1
 # SENTRY - error monitoring
 #
 SENTRY_DSN = config("SENTRY_DSN", None)
-RELEASE = "v1.35.0"  # get_current_version()
+RELEASE = "v2.0.0-dev"  # get_current_version()
 
 PRIVATE_MEDIA_ROOT = os.path.join(BASE_DIR, "private_media")
 FILER_ROOT = os.path.join(BASE_DIR, "media", "filer")
