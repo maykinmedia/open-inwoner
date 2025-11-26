7f68b01037069d8713e8de3f2b907c112691cfac
Sidney Richards <sidney@maykinmedia.nl>
2025-11-26 15:57:49 +0100

    feat: properly sync all ZGW fields during catalogus sync and give more feedback

--- src/open_inwoner/openzaak/management/commands/zgw_import_data.py
+++ src/open_inwoner/openzaak/management/commands/zgw_import_data.py
@@ -2,79 +2,28 @@ from django.core.management.base import BaseCommand
 
 from open_inwoner.openzaak.models import ZGWApiGroupConfig
 from open_inwoner.openzaak.zgw_imports import (
-    import_catalog_configs,
-    import_zaaktype_configs,
-    import_zaaktype_informatieobjecttype_configs,
-    import_zaaktype_resultaattype_configs,
-    import_zaaktype_statustype_configs,
+    ZGWCatalogusImporter,
 )
 
 
 class Command(BaseCommand):
     help = "Import ZGW catalog data"
 
-    def log_supplement_imports_to_stdout(
-        self, import_func: callable, config_type: str
-    ) -> None:
-        """
-        Convenience function for logging zaaktype config types to stdout
-
-        Example input:
-            import_func=import_zaaktype_informatieobjecttype_configs
-            config_type="informatiebjecttype"
-
-        Example output:
-            imported 3 new zaaktype-informatiebjecttype configs
-            AAA - zaaktype-aaa
-              info-aaa-1
-              info-aaa-2
-            BBB - zaaktype-bbb
-              info-bbb
-        """
-        imported = import_func()
-
-        count = sum(len(t[1]) for t in imported)
-        self.stdout.write(f"imported {count} new zaaktype-{config_type} configs")
-
-        for ztc, config_types in sorted(imported, key=lambda t: str(t[0])):
-            self.stdout.write(str(ztc))
-            for c in sorted(map(str, config_types)):
-                self.stdout.write(f"  {c}")
-
-        self.stdout.write("")
-
     def handle(self, *args, **options):
-        if ZGWApiGroupConfig.objects.count() == 0:
+        if not ZGWApiGroupConfig.objects.exists():
             self.stdout.write(
                 "Please define at least one ZGWApiGroupConfig before running this command."
             )
             return
 
-        # catalogus config
-        imported = import_catalog_configs()
+        importers = [
+            ZGWCatalogusImporter(api_group)
+            for api_group in ZGWApiGroupConfig.objects.all()
+        ]
 
-        self.stdout.write(f"imported {len(imported)} new catalogus configs")
-        for c in sorted(map(str, imported)):
-            self.stdout.write(c)
+        outputs: list[str] = []
+        for importer in importers:
+            result = importer.import_all()
+            outputs.append(result.pretty_print())
 
-        self.stdout.write("")
-
-        # zaaktype config
-        imported = import_zaaktype_configs()
-
-        self.stdout.write(f"imported {len(imported)} new zaaktype configs")
-        for c in sorted(map(str, imported)):
-            self.stdout.write(c)
-
-        self.stdout.write("")
-
-        # supplemental configs
-        self.log_supplement_imports_to_stdout(
-            import_zaaktype_informatieobjecttype_configs, "informatiebjecttype"
-        )
-        self.log_supplement_imports_to_stdout(
-            import_zaaktype_statustype_configs, "statustype"
-        )
-        self.log_supplement_imports_to_stdout(
-            import_zaaktype_resultaattype_configs, "resultaattype"
-        )
+        self.stdout.write("\n".join(outputs))
