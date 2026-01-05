16b65f32d7fc14f5f898666aa98d3763fd4b03e1
Paul Schilling <paul@maykinmedia.nl>
2026-01-05 14:49:04 +0100

    refactor: [#2088] refactor OTEL names to conform to spec

--- src/open_inwoner/cms/cases/metrics.py
+++ src/open_inwoner/cms/cases/metrics.py
@@ -4,8 +4,8 @@ meter = metrics.get_meter("open_inwoner.cases")
 
 
 case_list_views = meter.create_counter(
-    "cases.list_views",
-    unit="1",
+    "oip.cases.list_views",
+    unit="{view}",
     description=(
         "Number of times users view the cases list. "
         "Attributes: list_size (int), page (int)"
@@ -13,26 +13,26 @@ case_list_views = meter.create_counter(
 )
 
 case_detail_views = meter.create_counter(
-    "cases.detail_views",
-    unit="1",
+    "oip.cases.detail_views",
+    unit="{view}",
     description="Number of times users view case details",
 )
 
 case_document_downloads = meter.create_counter(
-    "cases.document_downloads",
-    unit="1",
+    "oip.cases.document_downloads",
+    unit="{download}",
     description="Number of times users download case documents",
 )
 
 case_document_uploads = meter.create_counter(
-    "cases.document_uploads",
-    unit="1",
+    "oip.cases.document_uploads",
+    unit="{upload}",
     description="Number of times users upload documents to cases",
 )
 
 case_contact_form_registrations = meter.create_counter(
-    "cases.contact_form.registrations",
-    unit="1",
+    "oip.cases.contact_form.registrations",
+    unit="{registration}",
     description=(
         "Contact form submissions. "
         "Attributes: channel ('email' or 'api'), success (bool)"
--- src/open_inwoner/openzaak/metrics.py
+++ src/open_inwoner/openzaak/metrics.py
@@ -3,8 +3,8 @@ from opentelemetry import metrics
 meter = metrics.get_meter("open_inwoner.openzaak")
 
 webhook_notifications_received = meter.create_counter(
-    "webhooks.notifications_received",
-    unit="1",
+    "oip.webhooks.notifications_received",
+    unit="{notification}",
     description=(
         "Number of webhook notifications received. "
         "Attributes: channel (str), result (str)"
@@ -12,8 +12,8 @@ webhook_notifications_received = meter.create_counter(
 )
 
 webhook_notifications_processed = meter.create_counter(
-    "webhooks.notifications_processed",
-    unit="1",
+    "oip.webhooks.notifications_processed",
+    unit="{notification}",
     description=(
         "Number of webhook notifications successfully processed. "
         "Attributes: channel (str), resource (str)"
@@ -21,8 +21,8 @@ webhook_notifications_processed = meter.create_counter(
 )
 
 webhook_notification_emails_sent = meter.create_counter(
-    "webhooks.notification_emails_sent",
-    unit="1",
+    "oip.webhooks.notification_emails_sent",
+    unit="{email}",
     description=(
         "Number of notification emails sent to users. "
         "Attributes: notification_type (str), template_name (str)"
