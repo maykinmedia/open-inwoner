0f6cb61558c922f6d76c04e5d29e86ce7f2aeb81
Sidney Richards <sidney@maykinmedia.nl>
2026-01-08 09:27:54 +0100

    fixup! [WIP] sentry logging

--- src/open_inwoner/conf/base.py
+++ src/open_inwoner/conf/base.py
@@ -404,6 +404,7 @@ LOGGING = {
                 structlog.stdlib.add_logger_name,
                 structlog.stdlib.add_log_level,
                 structlog.stdlib.PositionalArgumentsFormatter(),
+                structlog.processors.format_exc_info,
             ],
         },
         "plain_console": {
@@ -528,7 +529,6 @@ structlog.configure(
         structlog.stdlib.add_log_level,
         structlog.stdlib.PositionalArgumentsFormatter(),
         structlog.processors.StackInfoRenderer(),
-        structlog.processors.format_exc_info,
         structlog.processors.UnicodeDecoder(),
         structlog.stdlib.ProcessorFormatter.wrap_for_formatter,
     ],
