090f632c323d82865a4c4453d595c94b97a7d889
Sidney Richards <sidney@maykinmedia.nl>
2026-01-19 15:11:37 +0100

    Expose API data in mijn product list and detail

--- src/open_inwoner/mijn_producten/urls.py
+++ src/open_inwoner/mijn_producten/urls.py
@@ -6,5 +6,7 @@ app_name = "mijn_producten"
 
 urlpatterns = [
     path("", ProductListView.as_view(), name="mijn-product-list"),
-    path("<int:pk>/", ProductDetailView.as_view(), name="mijn-product-detail"),
+    path(
+        "<uuid:product_uuid>/", ProductDetailView.as_view(), name="mijn-product-detail"
+    ),
 ]
