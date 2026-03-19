5c0fdb64015f7fd27b46901d520d051c9092f4b2
OlhaZahoruiko <olha@maykinmedia.nl>
2026-03-19 14:52:02 +0100

    :sparkles: [#2321] Implement a BRP interactive config check

--- src/open_inwoner/config_checks/admin_views.py
+++ src/open_inwoner/config_checks/admin_views.py
@@ -1,15 +1,17 @@
 from django.contrib.admin.views.decorators import staff_member_required
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
 
 from open_inwoner.openzaak.models import ZGWApiGroupConfig
 
+from .fetch_brp import FetchBRPConfigCheck
 from .fetch_cases import FetchCasesConfigCheck
-from .forms import FetchCasesConfigCheckParams
+from .forms import FetchBRPConfigCheckParams, FetchCasesConfigCheckParams
 
 
 @staff_member_required
 def run_fetch_cases_check(request, api_group_id):
-    api_group = ZGWApiGroupConfig.objects.get(pk=api_group_id)
+    api_group = get_object_or_404(ZGWApiGroupConfig, pk=api_group_id)
 
     result = None
 
@@ -28,10 +30,38 @@ def run_fetch_cases_check(request, api_group_id):
 
     return render(
         request,
-        "admin/run_fetch_cases_check.html",
+        "admin/run_config_check.html",
         {
             "form": form,
             "result": result,
-            "api_group": api_group,
+            "title": f"Fetch cases check for {api_group.name}",
+            "back_url": reverse("admin:openzaak_openzaakconfig_changelist"),
+        },
+    )
+
+
+@staff_member_required
+def run_fetch_brp_check(request):
+    result = None
+
+    if request.method == "POST":
+        form = FetchBRPConfigCheckParams(request.POST)
+
+        if form.is_valid():
+            bsn = form.cleaned_data["bsn"]
+
+            check = FetchBRPConfigCheck(bsn)
+            result = check()
+    else:
+        form = FetchBRPConfigCheckParams()
+
+    return render(
+        request,
+        "admin/run_config_check.html",
+        {
+            "form": form,
+            "result": result,
+            "title": "Fetch BRP check",
+            "back_url": reverse("admin:haalcentraal_haalcentraalconfig_change"),
         },
     )
