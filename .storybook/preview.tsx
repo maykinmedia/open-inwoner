423064e8e65e3610f2d39b596a0efcaa208ec1eb
Stefan <stefan@maykinmedia.nl>
2026-04-13 16:56:29 +0200

    :lock: [#2412] Bumb vite, storybook, lodash and lodash-es to secure versions

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -13,13 +13,22 @@ const preview: Preview = {
   parameters: {
     reactIntl,
     controls: { matchers: { color: /(background|color)$/i, date: /Date$/ } },
+
     options: {
       storySort: {
         method: 'alphabetical',
         order: ['Introduction', 'Developers', 'Preact'],
       },
     },
+
     layout: 'centered',
+
+    a11y: {
+      // 'todo' - show a11y violations in the test UI only
+      // 'error' - fail CI on a11y violations
+      // 'off' - skip a11y checks entirely
+      test: 'todo',
+    },
   },
   tags: ['autodocs'],
   initialGlobals: {
