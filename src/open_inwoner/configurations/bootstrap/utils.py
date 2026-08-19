a46ed01dcc2980fa68c02312724825548d62be57
Paul Schilling <paul@maykinmedia.nl>
2026-08-19 10:44:18 +0100

    chore: [#2594] remove old-format setup-configuration steps

--- src/open_inwoner/configurations/bootstrap/utils.py
+++ src/open_inwoner/configurations/bootstrap/utils.py
@@ -1,22 +1,4 @@
-import structlog
 from zgw_consumers.models import Service
-
-logger = structlog.stdlib.get_logger(__name__)
-
-
-def convert_setting_to_model_field_name(setting: str, namespace: str) -> str:
-    return setting.split(f"{namespace}_", 1)[1].lower()
-
-
-def log_form_errors(config_step, form):
-    logger.error(
-        "Bootstrap configuration problems",
-        configuration_step=config_step.verbose_name,
-    )
-    for field, errors in form.errors.items():
-        logger.error(
-            "Bootstrap validation error", field=field, errors="; ".join(errors)
-        )
 
 
 def get_service(slug: str) -> Service:
