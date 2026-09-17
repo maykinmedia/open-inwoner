11a3995a6b46bd429885dff88d5fd6c19c31f143
nadia2603 <nadia@maykinmedia.nl>
2026-09-17 11:23:52 +0200

    fix: [#2804] hide spinner after timeout and show error

--- src/open_inwoner/js/components/index.js
+++ src/open_inwoner/js/components/index.js
@@ -58,6 +58,9 @@ htmx.config.allowEval = false;
 // injecting a style element is problematic with CSP
 htmx.config.includeIndicatorStyles = false;
 
+// Set default timeout of 5s;
+htmx.config.timeout = 5000;
+
 // define selectors and callables to apply after we loaded a html fragment
 const elementWrappers = [
   [CookieBanner.selector, (elt) => new CookieBanner(elt)],
